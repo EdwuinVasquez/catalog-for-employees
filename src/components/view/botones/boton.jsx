696887ae1e7df059afd98e33755e6f6a8c004642
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-06-15 07:48:35 -0500

    creacion del conponente boton
    El folder "view/botones" va a contener el archivo jsx de los
    botones que se van a manejar en la app.
    El folder "style/botones" va a contener el archivo css de los
    botones.
    creacion de un boton sencillo aun no tiene props ni hoock

---
+++ src/components/view/botones/boton.jsx
@@ -0,0 +1,14 @@
+//importacion de librerias
+import {React } from "react";
+import '../../style/botones/boton.css';
+
+export function BotonSencillo({titulo, icono, tipo}) {
+	return(
+		<>
+			<button className="botonSencillo" title="" type="submit" >
+				<span>hh</span>
+		  </button>
+		</>
+	);
+};
+
