9ca5e32e80d5c5ac092eea62f5423b66cbd59435
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-06-21 08:13:34 -0500

    Se creo un archivo de pagina principal
    este va a contener la pagina raiz de la pagina desde alli se podra
    aceder a cualquier parte de la aplicacion, hasta el momento solo
    funciona como un puente entre los diferentes componentes ya que
    esta pagina debe ser discutidad para saber especificamente
    debe de llevar.

---
+++ src/components/view/paginaMain/index.jsx
@@ -0,0 +1,38 @@
+//importacion de librerias
+import { React } from "react";
+import { Route, Routes, Link } from "react-router-dom";
+import { BotonSencillo } from "../botones/botonSencillo.jsx";
+import { Logo } from "../logos/logo.jsx";
+import { TituloCentradolo } from "../titulos/tituloCentrado.jsx";
+import { SubtituloCentrado } from "../subtitulos/subtituloCentrado.jsx";
+import '../../style/forms/inicioSesionForm.css'
+import { RegistroFomulario } from "../forms/registroForm.jsx";
+import { LoginFomulario } from "../forms/inicioSesionForm.jsx";
+
+export function Index({titulo, icono, tipo}) {
+  return(
+		<>
+		<div className="contenedor--centrado">
+			<form className="loginFomulario">
+				<Logo></Logo>
+				<TituloCentradolo texto="Rimoplasticas S.A"></TituloCentradolo>
+				<SubtituloCentrado 
+					texto="Bienvenido al catalogo de los empleados, antes de empezar ingrese a su cuenta"
+					></SubtituloCentrado>
+				<Link to="/login">
+					<BotonSencillo texto="Ingresar" manejarClik={() => {console.log("b")}}></BotonSencillo>
+				</Link>
+				<Link to="/registro">
+					<BotonSencillo texto="Registrarse" manejarClik={() => {console.log("a")}}></BotonSencillo>
+				</Link>
+			</form>
+		</div>
+
+		<Routes>
+    	<Route path="/login/*" element={<LoginFomulario />} />
+    	<Route path="/registro/*" element={<RegistroFomulario />} />
+			<Route path="*" element={null}/>
+    </Routes>
+		</>
+	);
+};
