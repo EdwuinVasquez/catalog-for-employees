715b466d4c41a8c1e9f6e5cd72177bc018afb76b
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-06-21 08:18:28 -0500

    Agregar react-router-dom
    se agrego el router-dom para poder navegar entre las diferentes paginas
    o components sin tener que recargar la pagina, aun asi en las seciones y
    tokents se hara uso de encriptacion de clave y uso de localStorage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import { LoginFomulario } from "./components/view/forms/inicioSesionForm.jsx";
 import { RegistroFomulario } from "./components/view/forms/registroForm.jsx";
+import { MenuSuperAdmin } from "./components/view/menus/menuSuperAdmin.jsx";
+import { Index } from "./components/view/paginaMain/index.jsx";
+import { Route, Routes } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <LoginFomulario></LoginFomulario>
-      <RegistroFomulario></RegistroFomulario>
+      <MenuSuperAdmin></MenuSuperAdmin>
+      {/* <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/login/*" element={<LoginFomulario />} />
+        <Route path="/registro/*" element={<RegistroFomulario />} />
+        <Route path="*" element={<RegistroFomulario />} />
+      </Routes> */}
     </div>
   );
 }
