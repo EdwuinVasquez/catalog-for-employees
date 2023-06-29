52233bae141b70a9d946a8b33e7b0ae47e37f2b8
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-06-29 08:10:33 -0500

    Nueva solicitud agregada
    se agrego una solicitud de axios para modificar los empleados a un
    estado bloqueado o activo.

--- src/backend/class/admin.js
+++ src/backend/class/admin.js
@@ -1,9 +1,11 @@
 import { axiosGet } from "../api/axiosGet.js";
 import { axiosPost } from "../api/axiosPost.js";
+import { axiosPut } from "../api/axiosPut.js";
 import { nuevoHash, compararHash } from "./bcrypt.js";
 import { usuario } from "./usuario.js"
 const classAxiosGet = new axiosGet();
 const classAxiosPost = new axiosPost();
+const classAxiosPut = new axiosPut();
 
 export class admin extends usuario  {
   constructor() {
@@ -13,5 +15,10 @@ export class admin extends usuario  {
 
   async usuarios(usuario = 0) {
     return await classAxiosGet.obtenerUsuario(usuario);
+  }
+
+  // PUT
+  async modificarEstadoUsuario(nuevoEstado = {}) {
+    return await classAxiosPut.cambiarEstadoUsuario(nuevoEstado);
   }
 }
