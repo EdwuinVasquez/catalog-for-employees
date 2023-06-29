eaaccafc161aa2f7a0377a83fdc44f60bf5f5f48
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-06-29 08:04:59 -0500

    Agregar el estado global y mas rutas
    Se configuro la app para usar el estado global de la aplicacion
    y usar para las rutas privadas, al igual que se agregaron nuevas
    rutas en la app.

--- src/App.js
+++ src/App.js
@@ -2,31 +2,44 @@ import { LoginFomulario } from "./components/view/forms/inicioSesionForm.jsx";
 import { RegistroFomulario } from "./components/view/forms/registroForm.jsx";
 import { MenuSuperAdmin } from "./components/view/menus/menuSuperAdmin.jsx";
 import { Index } from "./components/view/paginaMain/index.jsx";
+import { Process } from "./components/view/paginaMain/process.jsx";
 import { EmpleadosLista } from "./components/view/paginaMain/paginasAdmin/empleadosLista.jsx";
 import { Route, Routes, Outlet, Navigate } from "react-router-dom";
+import { useDataContex } from "./components/view/contex.jsx";
 import './App.css';
 
 function App() {
-
-  const loggin = "ad";
-
+	const { contexUsuario, setContexUsuario, contexMenu, setContexMenu } = useDataContex();
+  
   return (
     <div className="App">
-      <Routes>
-        <Route path="">
-          <Route path="/" element={<Index />} />
-          <Route path="/login/*" element={<LoginFomulario />} />
-          <Route path="/registro/*" element={<RegistroFomulario />} />
-          <Route path="*" element={<h1>404 No found</h1>} />
-        </Route>
-        <Route path="/admin/*">
-          <Route path="home" element={loggin == "ad" ? (<MenuSuperAdmin />) : ( <Navigate to="/login"/>)} />
-          <Route path="venta" element={loggin == "ad" ? (<MenuSuperAdmin />) : ( <Navigate to="/login"/>)} />
-          <Route path="empleados/lista" element={loggin == "ad" ? (<EmpleadosLista />) : ( <Navigate to="/login"/>)} />
-          <Route path="*" element={<h1>404 No found</h1>} />
-        </Route>
-      </Routes>
-      <Outlet></Outlet>
+      <div className={contexMenu === false ? "paginaMenu--desactiva" : "paginaMenu--activa"}>
+        <Routes>
+          <Route path=""></Route>
+          <Route path="/admin/*" element={contexUsuario == "ADMIN" || contexUsuario == "SUPER" ? (<MenuSuperAdmin />) : ( <Navigate to="/login"/>)}></Route>
+        </Routes>
+      </div>
+      <div className="paginaContenido">
+        <div className="paginaContenido--menu"></div>
+        <div className="paginaContenido--informacion">
+          <Routes>
+            <Route path="">
+              <Route path="/" element={<Index />} />
+              <Route path="/login/*" element={<LoginFomulario />} />
+              <Route path="/registro/*" element={<RegistroFomulario />} />
+              <Route path="/process/*" element={<Process />} />
+              <Route path="*" element={<h1>404 No found index</h1>} />
+            </Route>
+            <Route path="/admin/*">
+              <Route path="" element={contexUsuario == "ADMIN" || contexUsuario == "SUPER" ? (<h1>home</h1>) : ( <Navigate to="/process"/>)} />
+              <Route path="venta" element={contexUsuario == "ADMIN" || contexUsuario == "SUPER" ? (<h1>ventas</h1>) : ( <Navigate to="/process"/>)} />
+              <Route path="empleados/lista" element={contexUsuario == "ADMIN" || contexUsuario == "SUPER" ? (<EmpleadosLista />) : ( <Navigate to="/process"/>)} />
+              <Route path="*" element={<h1>404 No found administrador</h1>} />
+            </Route>
+          </Routes>
+        </div>
+        
+      </div>
     </div>
   );
 }
