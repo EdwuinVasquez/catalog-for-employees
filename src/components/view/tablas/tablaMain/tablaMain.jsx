229d9c6c405d92c99b9d2a39cf4ed2457b8edd3f
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-06-29 08:29:50 -0500

    Modificacion de estado y props
    se realizaron modificaciones en los props que envia a los componentes
    que usa y se realizaron ajustes en el useState de el buscador

--- src/components/view/tablas/tablaMain/tablaMain.jsx
+++ src/components/view/tablas/tablaMain/tablaMain.jsx
@@ -1,14 +1,26 @@
 //importacion de librerias
-import { React } from "react";
+import { React, useEffect, useState } from "react";
 import { TablaCabezera } from "./componTabla/cabezera";
 import "../../../style/tabla/tablaMain/contenedor.css"
 import { TablaCotenido } from "./componTabla/contenido";
 
-export function TablaMain({buscadorNombre, buscadorTitulo, tablaTitulos, tablaContenido}) {
+export function TablaMain({buscadorNombre, buscadorTitulo, tablaTitulos, tablaContenido, actualizar}) {
+  const [contenidoFilter, setContenidoFilter] = useState([[]]);
+  const realizarBusqueda = (e) =>{
+    let valueBuscador = e.target.value.trim();
+    let datosFiltrados = tablaContenido.filter(
+      tupla => (tupla[0]['valor'].includes(valueBuscador) || tupla[1]['valor'].includes(valueBuscador)) 
+    );
+    console.clear();
+    if(datosFiltrados.length > 0) setContenidoFilter(datosFiltrados);
+  }
+
   return(
+    <>
     <div className="tablaMain">
-      <TablaCabezera buscador={buscadorNombre} titulo={buscadorTitulo}></TablaCabezera>
-      <TablaCotenido titulos={tablaTitulos} contenido={tablaContenido}></TablaCotenido>
+      <TablaCabezera buscador={buscadorNombre} titulo={buscadorTitulo} manejarClick={realizarBusqueda}></TablaCabezera>
+      <TablaCotenido titulos={tablaTitulos} actualizar={actualizar} contenido={(contenidoFilter[0].length >= 2 ? contenidoFilter : tablaContenido)}></TablaCotenido>
     </div>
+    </>
 	);
 };
