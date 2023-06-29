426295d9803218ee53d1a56f2b9d9252cf48a414
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-06-29 08:46:20 -0500

    manejo de props y useEffect
    se retiro el useEffect y se modifico el procedimiento de
    la modificacion de empledos tambien se modificaron algunos props.

--- src/components/view/paginaMain/paginasAdmin/empleadosLista.jsx
+++ src/components/view/paginaMain/paginasAdmin/empleadosLista.jsx
@@ -1,83 +1,124 @@
 //importacion de librerias
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import { TablaMain } from "../../tablas/tablaMain/tablaMain.jsx";
 import { admin } from "../../../../backend/class/admin.js"
+import { useDataContex } from "../../contex.jsx"
 const classAdmin = new admin();
 
-export function EmpleadosLista({titulo, icono, tipo}) {
-	const [datosMain, setDatosMain] = useState
-	([[{
-		key: 1234,
-		tipo: "normal",
-		valor: "",
-		img: "",
-		subClase: ""
-	}]]);
+export function EmpleadosLista() {
+	const [datosMain, setDatosMain] = useState();
 	
-	const titulosTabla = ["Cedula", "Nombre", "N°compras", "Ingreso", "Contato", "Estado", "Operacion"]
+	const titulosTabla = ["Cedula", "Nombre", "N°compras", "Ingreso", "Contato", "Operacion", "Estado"]
 
 	const generarLista = async () => {
 		classAdmin.usuarios()
 		.then(resultado => {
 			if(resultado === false){
-				console.log("d");
+				console.clear();
+				throw new Error("No hay resultados almacenados");
 			}
-			let nuevosDatos = resultado.map((valor) =>
+			const empleadosLista = resultado.filter(tupla => tupla["ROL"] === "EMPLEADO");
+			const nuevosDatos = empleadosLista.map((valor) =>
 				[{
 					key: valor["CEDULA"],
+					id: valor["CEDULA"],
 					tipo: "normal",
 					valor: valor["CEDULA"],
 					img: "",
-					subClase: ""
+					subClase: "",
+					operacion: "",
+					parametro: ""
 				},{
 					key: valor["CEDULA"],
+					id: valor["CEDULA"],
 					tipo: "normal",
 					valor: valor["NOMBRE"],
 					img: "",
-					subClase: ""
+					subClase: "",
+					operacion: "",
+					parametro: ""
 				},{
 					key: valor["CEDULA"],
-					tipo: "imag",
+					id: valor["CEDULA"],
+					tipo: "normal",
 					valor: valor["NUMERO_COMPRAS"],
 					img: "",
-					subClase: ""
+					subClase: "",
+					operacion: "",
+					parametro: ""
 				},{
 					key: valor["CEDULA"],
+					id: valor["CEDULA"],
 					tipo: "normal",
 					valor: valor["ULTIMO_INGRESO"],
 					img: "",
-					subClase: ""
+					subClase: "",
+					operacion: "",
+					parametro: ""
 				},{
 					key: valor["CEDULA"],
+					id: valor["CEDULA"],
 					tipo: "normal",
 					valor: valor["CONTACTO"],
 					img: "",
-					subClase: ""
+					subClase: "",
+					operacion: "",
+					parametro: ""
 				},{
 					key: valor["CEDULA"],
+					id: valor["CEDULA"],
+					tipo: "boton",
+					valor: "Modificar",
+					img: "",
+					subClase: "",
+					operacion: modificarEstadoEmpleado,
+					parametro: valor["USUARIO_ACTIVADO"] == 1 ? 0 : 1
+				},{
+					key: valor["CEDULA"],
+					id: valor["CEDULA"],
 					tipo: "estado",
 					valor: valor["USUARIO_ACTIVADO"],
 					img: "",
-					subClase: valor["USUARIO_ACTIVADO"]
+					subClase: valor["USUARIO_ACTIVADO"],
+					operacion: "",
+					parametro: ""
 				}]
 			);
-			setDatosMain(nuevosDatos);
+			if(nuevosDatos.length > 0) setDatosMain(nuevosDatos);
+		}).catch(function (error) {
+			setDatosMain([[]]);
+			console.error(error);
+		});
+	}
+
+	const modificarEstadoEmpleado = async (cedula, dato) =>{
+		const nuevoEstado = {
+			cedula: cedula,
+			estado: dato,
+		}
+
+		classAdmin.modificarEstadoUsuario(nuevoEstado)
+		.then(resultado =>{
+			if(resultado === false){
+				console.clear();
+				throw new Error("No hay resultados almacenados");
+			}
+			
+			generarLista();
 		}).catch(function (error) {
 			console.error(error);
 		});
 	}
 
-	
-
   return(
 		<>
 			<TablaMain 
-				buscadorNombre="Nombre" 
-				buscadorTitulo="Empleados" 
+				buscadorNombre="Cedula o nombre" 
+				buscadorTitulo="Empleados"  
+				actualizar={generarLista}
 				tablaTitulos={titulosTabla}
 				tablaContenido={datosMain} >
 			</TablaMain>
-			<button onClick={() => generarLista()}> ass</button>
 		</>
 	);
 };
