de8ca8eb19f53369bd5c16a077d5c5b229a11cb2
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-06-29 08:14:50 -0500

    Nuevo archivo process
    el archivo es un puente entre la sesion y su cuennta si existe
    un usuario con una cuenta activa en la app el sistema lo redigira a la
    pantalla principal de lo contrario lo devolvera al index de la pagina

---
+++ src/components/view/paginaMain/process.jsx
@@ -0,0 +1,64 @@
+//importacion de librerias
+import { React, useEffect } from "react";
+import { useDataContex } from "../contex";
+import { usuario } from "../../../backend/class/usuario";
+const classUsuario = new usuario();
+
+export function Process() {
+	const { urlBase, setContexUsuario, setContexUsuarioLogin } = useDataContex();
+	
+	useEffect(() => {
+		try {
+			let datosAlmacenados = JSON.parse(localStorage.getItem("usuario"));
+		  classUsuario.ingresar(datosAlmacenados['cedula'], datosAlmacenados['clave'])
+		  .then(resultado =>{
+		  	if(resultado[0]['CEDULA'] == undefined){
+		  		window.location.href = `${urlBase}`;
+		  		throw new Error("NO SE ENCONTRARON DATOS");
+		  	}
+		  	
+		  	let sesion = {
+		  		cedula: resultado[0]['CEDULA'],
+		  		clave: resultado[0]['CLAVE'],
+		  		nombre: resultado[0]['NOMBRE'],
+		  		rol: resultado[0]['ROL'],
+		  		conctacto: resultado[0]['CONTACTO']
+		  	};
+		  	localStorage.removeItem("usuario");
+		  	localStorage.setItem("usuario", JSON.stringify(sesion));
+  
+		  	setContexUsuario(resultado[0]['ROL']);
+		  	setContexUsuarioLogin(sesion);
+		  	
+		  	switch (resultado[0]['ROL']) {
+		  		case "SUPER":
+		  			window.location.href = `${urlBase}admin`;
+		  			break;
+		  		case "ADMIN":
+		  			window.location.href = `${urlBase}admin`;
+		  			break;
+		  		case "V-V":
+		  			window.location.href = `${urlBase}admin`;
+		  			break;
+		  		case "EMPLEADO":
+		  			window.location.href = `${urlBase}emple`;
+		  			break;
+		  		default:
+		  			window.location.href = `${urlBase}`;
+		  			break;
+		  	}
+		  })
+		  .catch(function (error) {
+        console.error(error);
+      });
+		} catch (error) {
+			window.location.href = `${urlBase}`;
+		}
+	});
+
+  return(
+		<>
+		<h1>Procesando</h1>
+		</>
+	);
+};
