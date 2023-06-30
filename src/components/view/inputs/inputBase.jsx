1adb1b3ce4fd039bf5f402c2bbd676f6422fcd09
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-06-30 07:36:51 -0500

    Nuevo estado
    Se inluyo un nuevo estado vacio que funciona para
    incluir la nueva clase "inputBase--vacio" esta clase sera
    otrogada al abrir la pantalla esto con el fin de dar una
    sensacion mas amigable al usuario.

--- src/components/view/inputs/inputBase.jsx
+++ src/components/view/inputs/inputBase.jsx
@@ -4,10 +4,12 @@ import '../../style/inputs/inputBase.css';
 
 export function CampoVerificado({id, tipo, texto, expresion, manejarCambio}) {
 	const [datosValido, setDatosValido] = useState(false);
+	const [valueCampo, setValueCampo] = useState(true);
 
   const value = (event) =>{
     let value = event.target.value;
     setDatosValido(expresion.test(value));
+    setValueCampo(value.trim() == "");
     manejarCambio(value.trim(), expresion.test(value), id);
   }
 
@@ -15,7 +17,9 @@ export function CampoVerificado({id, tipo, texto, expresion, manejarCambio}) {
 		<>
 		<div className="campoVerificado">
       <input className="campoVerificado__campo" onChange={value} placeholder="" type={tipo} required="" autoComplete="off" />
-      <label className={"campoVerificado__texto  " + (datosValido ? "campoVerificado__texto--verde" : "campoVerificado__texto--rojo")} >{texto}</label>
+      <label 
+        className={"campoVerificado__texto  " + (datosValido ? "campoVerificado__texto--verde" : "campoVerificado__texto--rojo") + (valueCampo ? "  campoVerificado__texto--vacio" : "  ")} 
+        >{texto}</label>
     </div>
 		</>
 	);
