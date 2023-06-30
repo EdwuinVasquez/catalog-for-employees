30911fa66e5ea76eafbdf73753b394eb5bc91ab5
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-06-30 15:17:25 -0500

    Ajustes de stylo y rutas
    Se agregaron nuvas rutas y se añadieron nuevas clases al archivo css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import { Route, Routes, Outlet, Navigate } from "react-router-dom";
 import { LoginFomulario } from "./components/view/forms/inicioSesionForm.jsx";
 import { RegistroFomulario } from "./components/view/forms/registroForm.jsx";
 import { MenuSuperAdmin } from "./components/view/menus/menuSuperAdmin.jsx";
 import { Index } from "./components/view/paginaMain/index.jsx";
 import { Process } from "./components/view/paginaMain/process.jsx";
 import { EmpleadosLista } from "./components/view/paginaMain/paginasAdmin/empleadosLista.jsx";
-import { Route, Routes, Outlet, Navigate } from "react-router-dom";
 import { useDataContex } from "./components/view/contex.jsx";
+import { RegistroLista } from "./components/view/paginaMain/paginasAdmin/registrosLista.jsx";
 import './App.css';
+import { ProductoLista } from "./components/view/paginaMain/paginasAdmin/productoLista.jsx";
 
 function App() {
 	const { contexUsuario, setContexUsuario, contexMenu, setContexMenu } = useDataContex();
@@ -33,7 +35,16 @@ function App() {
             <Route path="/admin/*">
               <Route path="" element={contexUsuario == "ADMIN" || contexUsuario == "SUPER" ? (<h1>home</h1>) : ( <Navigate to="/process"/>)} />
               <Route path="venta" element={contexUsuario == "ADMIN" || contexUsuario == "SUPER" ? (<h1>ventas</h1>) : ( <Navigate to="/process"/>)} />
-              <Route path="empleados/lista" element={contexUsuario == "ADMIN" || contexUsuario == "SUPER" ? (<EmpleadosLista />) : ( <Navigate to="/process"/>)} />
+              <Route path="producto/*">
+                <Route path="lista" element={contexUsuario == "ADMIN" || contexUsuario == "SUPER" ? (<ProductoLista />) : ( <Navigate to="/process"/>)} />
+                <Route path="categorias" element={contexUsuario == "ADMIN" || contexUsuario == "SUPER" ? (<h1> Lista categorias</h1>) : ( <Navigate to="/process"/>)} />
+                <Route path="ajustes" element={contexUsuario == "ADMIN" || contexUsuario == "SUPER" ? (<h1> Ajustes</h1>) : ( <Navigate to="/process"/>)} />
+              </Route>
+              <Route path="empleados/*">
+                <Route path="lista" element={contexUsuario == "ADMIN" || contexUsuario == "SUPER" ? (<EmpleadosLista />) : ( <Navigate to="/process"/>)} />
+                <Route path="nuevosRegistros" element={contexUsuario == "ADMIN" || contexUsuario == "SUPER" ? (<RegistroLista />) : ( <Navigate to="/process"/>)} />
+                <Route path="ajustes" element={contexUsuario == "ADMIN" || contexUsuario == "SUPER" ? (<h1> Ajustes empleado</h1>) : ( <Navigate to="/process"/>)} />
+              </Route>
               <Route path="*" element={<h1>404 No found administrador</h1>} />
             </Route>
           </Routes>
