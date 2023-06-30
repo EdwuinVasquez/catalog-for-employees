8a9810e19ba401d84ab10ee2ba2eec9d7f217407
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-06-30 15:28:04 -0500

    finalizacion del componente menuAdmin y ajustes
    se finalizo el menu de administrador con sus rutas, nombre y acceso a
    sesiones solo aptas para usuarios de tipo Super.
    Las opciones se cambio el tamaño de la letra

--- src/components/view/menus/menuSuperAdmin.jsx
+++ src/components/view/menus/menuSuperAdmin.jsx
@@ -11,6 +11,20 @@ import { useDataContex } from "../contex.jsx";
 export function MenuSuperAdmin() {
 	const { contexUsuario, setContexUsuario, contexMenu, setContexMenu } = useDataContex();
 
+	const adminSuper = () =>{
+		if(contexUsuario == "SUPER"){
+			return <>
+			<OpcionMenu opciones={true} icono="FaUserPlus" nombre="Usuario">
+				<ul className="opcion__subMenu">
+					<SubOpcionMenu ruta="usuarios/lista" nombre="Lista usuarios"></SubOpcionMenu>
+					<SubOpcionMenu ruta="usuarios/nuevo" nombre="Crear usuario"></SubOpcionMenu>
+					<SubOpcionMenu ruta="usuarios/eliminar" nombre="Eliminar usuario"></SubOpcionMenu>
+					<SubOpcionMenu ruta="usuarios/modificar" nombre="Modificar usuario"></SubOpcionMenu>
+				</ul>
+			</OpcionMenu>
+			</>
+		}
+	}
   return(
 		<>
 		<div className={contexMenu === false ? "menu--desactivado" : "menu--activado"}>
@@ -19,44 +33,45 @@ export function MenuSuperAdmin() {
 				<BotonMenu estado={contexMenu} ></BotonMenu>
 				<ul className="lista">
 					<OpcionMenu opciones={false} ruta="home" icono="FaHome" nombre="Inicio"></OpcionMenu>
-					<OpcionMenu opciones={false} ruta="venta" icono="FaMoneyBill" nombre="Ventas"></OpcionMenu>
-					<OpcionMenu opciones={true} ruta="reporte" icono="VscGraph" nombre="Reportes">
-						<ul className="opcion__subMenu">
-							<SubOpcionMenu ruta="reporte" nombre="Ventas"></SubOpcionMenu>
-							<SubOpcionMenu ruta="reporte" nombre="Productos"></SubOpcionMenu>
-							<SubOpcionMenu ruta="reporte" nombre="Empleados"></SubOpcionMenu>
-							<SubOpcionMenu ruta="reporte" nombre="Mes"></SubOpcionMenu>
-						</ul>
-					</OpcionMenu>
-					<OpcionMenu opciones={true} ruta="" icono="HiBuildingStorefront" nombre="Productos">
-						<ul className="opcion__subMenu">
-							<SubOpcionMenu nombre="Lista"></SubOpcionMenu>
-							<SubOpcionMenu nombre="Categorias"></SubOpcionMenu>
-							<SubOpcionMenu nombre="Ajustes"></SubOpcionMenu>
-						</ul>
-					</OpcionMenu>
-					<OpcionMenu opciones={true} ruta="" icono="RiUserFollowFill" nombre="Empleados">
+					<OpcionMenu opciones={true} icono="FaMoneyBill" nombre="Ventas">
 					<ul className="opcion__subMenu">
-							<SubOpcionMenu ruta="empleados/lista" nombre="Lista"></SubOpcionMenu>
-							<SubOpcionMenu ruta="empleados/nuevosRegistros" nombre="Sin verificar"></SubOpcionMenu>
-							<SubOpcionMenu ruta="empleados/ajustes" nombre="Ajustes"></SubOpcionMenu>
+							<SubOpcionMenu ruta="ventas/lista" nombre="Lista ventas"></SubOpcionMenu>
+							<SubOpcionMenu ruta="ventas/editar" nombre="Modificar venta"></SubOpcionMenu>
 						</ul>
 					</OpcionMenu>
-					<OpcionMenu opciones={true} ruta="" icono="FaUserPlus" nombre="Usuario">
-					<ul className="opcion__subMenu">
-							<SubOpcionMenu nombre="Crear usuario"></SubOpcionMenu>
-							<SubOpcionMenu nombre="Eliminar usuario"></SubOpcionMenu>
-							<SubOpcionMenu nombre="Modificar usuario"></SubOpcionMenu>
-						</ul>
-					</OpcionMenu>
-					<OpcionMenu opciones={true} ruta="" icono="AiFillAppstore" nombre="Aplicacion">
+					<OpcionMenu opciones={true} icono="VscGraph" nombre="Reportes">
 						<ul className="opcion__subMenu">
-							<SubOpcionMenu nombre="Ajustes"></SubOpcionMenu>
-							<SubOpcionMenu nombre="Alertas"></SubOpcionMenu>
+							<SubOpcionMenu ruta="reporte/ventas" nombre="Ventas"></SubOpcionMenu>
+							<SubOpcionMenu ruta="reporte/producto" nombre="Productos"></SubOpcionMenu>
+							<SubOpcionMenu ruta="reporte/empleado" nombre="Empleados"></SubOpcionMenu>
+							<SubOpcionMenu ruta="reporte/mes" nombre="Mes"></SubOpcionMenu>
 						</ul>
 					</OpcionMenu>
-					<OpcionMenu opciones={false} ruta="" icono="GrHistory" nombre="Historial"></OpcionMenu>
-					<OpcionMenu opciones={false} ruta="" icono="RiSettings4Fill" nombre="Configuracion"></OpcionMenu>
+					<OpcionMenu opciones={true} icono="HiBuildingStorefront" nombre="Productos">
+						<ul className="opcion__subMenu">
+							<SubOpcionMenu ruta="producto/lista" nombre="Lista productos"></SubOpcionMenu>
+							<SubOpcionMenu ruta="producto/categorias" nombre="Lista categorias"></SubOpcionMenu>
+							<SubOpcionMenu ruta="producto/ajustes" nombre="Editar producto"></SubOpcionMenu>
+						</ul>
+					</OpcionMenu>
+					<OpcionMenu opciones={true} icono="RiUserFollowFill" nombre="Empleados">
+					<ul className="opcion__subMenu">
+							<SubOpcionMenu ruta="empleados/lista" nombre="Lista empleados"></SubOpcionMenu>
+							<SubOpcionMenu ruta="empleados/nuevosRegistros" nombre="No verificados"></SubOpcionMenu>
+							<SubOpcionMenu ruta="empleados/token" nombre="Generar token"></SubOpcionMenu>
+							<SubOpcionMenu ruta="empleados/nuevo" nombre="Crear empleado"></SubOpcionMenu>
+							<SubOpcionMenu ruta="empleados/ajustes" nombre="Editar empleado"></SubOpcionMenu>
+						</ul>
+					</OpcionMenu>
+					{adminSuper()}
+					<OpcionMenu opciones={true} icono="AiFillAppstore" nombre="Aplicacion">
+						<ul className="opcion__subMenu">
+							<SubOpcionMenu ruta="aplicacion/tablon" nombre="Tablon"></SubOpcionMenu>
+							<SubOpcionMenu ruta="aplicacion/alertas" nombre="Alertas"></SubOpcionMenu>
+						</ul>
+					</OpcionMenu>
+					<OpcionMenu opciones={false} ruta="historial" icono="GrHistory" nombre="Historial"></OpcionMenu>
+					<OpcionMenu opciones={false} ruta="configuracion" icono="RiSettings4Fill" nombre="Configuracion"></OpcionMenu>
 				</ul>
 			</nav>
 		</div>
