e2fe4ef4517d950e087f7c84ce22347a942da690
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-07-12 17:13:03 -0500

    Uso de estado global cambio de props
    Ahora el componente usa un estado global al igual que uno local para
    su visualizacion, se modifico la forma de calcular el numero de item
    cargados en la compra, ahora se calcula directamente en el componente

--- src/components/view/badge/carrito.jsx
+++ src/components/view/badge/carrito.jsx
@@ -1,12 +1,34 @@
-//importacion de librerias
-import { React, useState } from "react";
+/*-- Librerias --*/
+import { React } from "react";
 import { Badge } from "@mui/material";
 import { BsCart3 } from "react-icons/bs";
 
-export function CarritoBoton({numero}) {
+/*-- clases y controladores --*/
+import { useDataContex } from "../contex";
+
+export function CarritoBoton({manejarClick}) {
+	/*-- Estado global --*/
+	const { carrito, setContexMenu } =  useDataContex();
+
+	/*-- Activar visor de carrito --*/
+	const procesarClick = (() => {
+		setContexMenu(false);
+		manejarClick(true);
+	})
+
+	/*-- Calcular numero de items --*/
+	const calcularItems = (() =>{
+		const total = carrito.reduce((valorAnterior, valorActual) => {
+			return valorAnterior + valorActual[0]["cantidad"];
+		}, 0);
+		return total;
+	});
+
   return(
-    <Badge color="primary" badgeContent={numero} showZero>
-  		<BsCart3></BsCart3>
+	<div onClick={procesarClick}>
+		<Badge color="primary" badgeContent={calcularItems()} showZero>
+			<BsCart3></BsCart3>
 		</Badge>
+	</div>
 	);
 };
