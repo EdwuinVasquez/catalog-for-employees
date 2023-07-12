961edda70fd04f7436f66394c2cf3fda6a9d8529
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-07-12 16:57:41 -0500

    Nuevo paquete incluido
    Se agrego el paquete de Notistack para uso de alertas,
    se implemente un provider en el index para el uso de
    las alertas.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, HashRouter } from "react-router-dom";
 import { DataContextProvider } from "./components/view/contex.jsx"
+import { SnackbarProvider, useSnackbar } from 'notistack';
 import './index.css';
 import App from './App';
 
@@ -9,9 +10,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HashRouter>
-      <DataContextProvider>
-        <App />
-      </DataContextProvider>
+      <SnackbarProvider>  
+        <DataContextProvider>
+          <App />
+        </DataContextProvider>
+      </SnackbarProvider>
     </HashRouter>
   </React.StrictMode>
 );
