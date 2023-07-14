5e6401c2ab636ef446a01175290ccbf98d20c815
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-07-14 15:24:25 -0500

    Activar buscador con tecla
    ahora el buscador funciona al oprimir la tecla enter

--- src/components/view/tablas/tablaMain/componTabla/buscador.jsx
+++ src/components/view/tablas/tablaMain/componTabla/buscador.jsx
@@ -1,18 +1,32 @@
-//importacion de librerias
+/*-- Estilos --*/
+import "../../../../style/tabla/tablaMain/buscador.css";
+
+/*-- Importaciones --*/
 import { React, useState } from "react";
+
+/*-- componentes --*/
 import { buscador } from "../../../logos/img.jsx";
-import "../../../../style/tabla/tablaMain/buscador.css"
 
 export function TablaBuscador({placeholder, manejarClick}) {
+  /*-- Contenido del buscador --*/
   const [buscar, setBuscar] = useState(null);
 
+  /*-- Actualizar buscador --*/
   const actualizarBuscador = (e) => {
     let value = e.target.value.trim();
     setBuscar(value)
   }
+
+  /*-- Activar busqueda por tecla enter --*/
+  function pulsar(e) {
+    if (e.keyCode === 13 && !e.shiftKey) {
+      manejarClick(buscar);
+    }
+  }
+  
   return(
     <div className="buscadorTabla">
-      <input className="buscadorTabla__input" type="search" onChange={actualizarBuscador} placeholder={placeholder} />
+      <input className="buscadorTabla__input" onKeyDown={pulsar}  type="search" onChange={actualizarBuscador} placeholder={placeholder} />
       <img className="buscadorTabla__img" src={buscador} onClick={() => manejarClick(buscar)} alt="" />
     </div>
 	);
