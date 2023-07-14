92fc68c9e2a3cb9ac2066b0f59143164f4276922
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-07-14 15:15:23 -0500

    Comentarios y props
    se agregaron algunos comentarios y ahora el componente
    recibe un props para ocultar la funcion de exportar datos

--- src/components/view/tablas/tablaMain/tablaMain.jsx
+++ src/components/view/tablas/tablaMain/tablaMain.jsx
@@ -1,11 +1,18 @@
-//importacion de librerias
-import { React, useEffect, useState } from "react";
-import { TablaCabezera } from "./componTabla/cabezera";
+/*-- Estilos --*/
 import "../../../style/tabla/tablaMain/contenedor.css"
+
+/*-- Importaciones --*/
+import { React, useState } from "react";
+
+/*-- Componentes --*/
+import { TablaCabezera } from "./componTabla/cabezera";
 import { TablaCotenido } from "./componTabla/contenido";
 
-export function TablaMain({buscadorNombre, buscadorTitulo, tablaTitulos, tablaContenido, actualizar}) {
+export function TablaMain({buscadorNombre, buscadorTitulo, tablaTitulos, tablaContenido, actualizar, oculto}) {
+  /*-- Lista de datos filtrados --*/
   const [contenidoFilter, setContenidoFilter] = useState([[]]);
+  
+  /*-- Buscar en el contenido de la tabla --*/
   const realizarBusqueda = (buscar) =>{
     try {
       let valueBuscador = buscar.trim();
@@ -22,7 +29,7 @@ export function TablaMain({buscadorNombre, buscadorTitulo, tablaTitulos, tablaCo
   return(
     <>
     <div className="tablaMain">
-      <TablaCabezera buscador={buscadorNombre} titulo={buscadorTitulo} manejarClick={realizarBusqueda}></TablaCabezera>
+      <TablaCabezera buscador={buscadorNombre} titulo={buscadorTitulo} manejarClick={realizarBusqueda} oculto={oculto}></TablaCabezera>
       <TablaCotenido titulos={tablaTitulos} actualizar={actualizar} contenido={(contenidoFilter[0].length >= 2 ? contenidoFilter : tablaContenido)}></TablaCotenido>
     </div>
     </>
