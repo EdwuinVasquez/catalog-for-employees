1359aaa8a8650e95d889f3855d28dde14a935947
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-08-04 14:05:34 -0500

    Archivos de uso general del software
    En estos tres archivos se encuentran variables o métodos generales
    que pueden usarse en varios componentes.
    
    En el archivo regex.js se puede encontrar las expresiones regulares
    para evaluar diferentes tipos de datos del sistema. Entre las
    Expresiones regulares encontradas están:
    
    - Nombre
    - Cédula
    - Número
    - Any
    - Contacto
    - Pregunta
    - Respuesta
    - Clave
    - Pesos
    
    En el archivo swetAlert2.js nos encontramos con diferentes tipos de
    alertas generadas por la librería swetAlert2, continuación se
    Describirá brevemente la función de cada alerta:
    
    - alertaClasica: Muestra un mensaje básico en el centro de la pantalla,
    esta necesita parámetros como el icono, mensaje, tiempo de duración.
    
    - alertaToast: Muestra un pequeño mensaje en la parte de la pantalla
    indicada, esta necesita parámetros como el icono, mensaje, tiempo de
    Duración, posición.
    
    - alertaConfirmar: Muestra una alerta la cual solicita que confirme o
    Rechacé una petición de alto riesgo a realizar, esta necesita parámetros
    como el icono, mensaje, tiempo de duración retorna un booleano.
    
    - alertaInput: Muestra una alerta la cual solicita un dato en pantalla,
    esta alerta funciona para modificar datos de una tabla sin la necesidad
    de redirigir a otra ventana, Esta solicita un mensaje de lo que solicita
    y retorna el dato ingresado en el input.
    
    - maximixarImagen: Muestra una imagen en una alerta, esta necesita
    Parámetros como URL de la imagen y nombre de la imagen.
    
    En el archivo funcioneGenerales.js encontramos métodos y variables
    usadas en muchas partes de la aplicación, a continuación se describirá un
    poco de lo que se encuentra en este archivo:  
    
    Variables:
    - listaOpciones: contiene las opciones de la pregunta de seguridad.
    
    Métodos:
    - formatearNumero: Recibe como parámetro un número entero, el cual será
    retornado con"." Cada 3 dígitos.
    - formatearTexto: Recibe una cadena de texto y un número, se evaluará si
    el texto supera la longitud indicada en el segundo parámetro, si es
    superada la recortará hasta el número indicado -5 y se agregarán "..."
    Para indicar que supera la longitud máxima y se retorna, si esto no
    ocurre y no supera el límite, se retornará el texto original.
    - precioVenta: Este método recibe el precio, IVA y descuento. Con los
    datos recibidos calculará el precio de venta del producto.
    - precioBase: Este método recibe el precio e IVA. Con los datos
    recibidos calculará el precio del producto incluyendo su precio con IVA.
    - nombreEstado: recibe el número el cual se retornara en texto el nombre
    del estado para Mostar en pantalla más información en el Readme.
    - subClase: recibe el número el cual se retornara en texto el nombre
    del estado para asignar una clase especifica al estado más información
    en el Readme.
    - cerrarAlertaAction: Retorna un botón para cerrar las alertas de
    notistack.
    - imprimir: despliega la pantalla de impresión en la pantalla.

---
+++ src/backend/funcioneGenerales.js
@@ -0,0 +1,104 @@
+import { regex } from "./regex";
+import { closeSnackbar } from "notistack";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { evaluate } from "mathjs";
+
+/*-- Lista de opciones para pregunta de seguridad */
+export const listaOpciones = [
+  [{ key: 1, valor: "En que ciudad naciste" }],
+  [{ key: 2, valor: "Cual es tu equipo deportivo favorito" }],
+  [{ key: 3, valor: "Cual es el nombre de tu ciudad natal" }],
+  [{ key: 4, valor: "Cual es tu deporte favorito" }],
+  [{ key: 5, valor: "Nombre de tu ultima mascota" }],
+  [{ key: 6, valor: "Cual es tu numero de identificación personal" }],
+  [{ key: 7, valor: "En que mes naciste" }],
+  [{ key: 8, valor: "En que año empezaste a trabajar en esta empresa" }],
+  [{ key: 9, valor: "Cual es tu numero de telefono" }],
+  [{ key: 10, valor: "En que dia naciste" }],
+  [{ key: 11, valor: "Cual es tu posicion en la linea de produccion" }],
+  [{ key: 12, valor: "Cual es el nombre de tu supervisor directo" }],
+];
+
+/*-- Formatear Numero en pesos --*/
+export const formatearNumero = (numero) => {
+  const expresion = regex.pesos;
+  const remplazo = "$1.";
+  return numero.toString().replace(expresion, remplazo);
+};
+
+export const formatearTexto = (texto, limite) => {
+  if (texto.length >= limite) {
+    return `${texto.slice(0, limite - 5)} ...`;
+  }
+  return texto;
+};
+
+/*-- Formatear a precio de venta --*/
+export const precioVenta = (precio, iva, descuento) => {
+  return Math.ceil(
+    evaluate(`(${precio}*1.${iva})-((${precio}*1.${iva})*${descuento}%)`)
+  );
+};
+
+/*-- Formatear a precio de Base --*/
+export const precioBase = (precio, iva) => {
+  return Math.ceil(evaluate(`(${precio}*1.${iva})`));
+};
+
+/*-- Asignar un nombre al estado --*/
+export const nombreEstado = (valor) => {
+  const dato = valor.toString();
+  switch (dato) {
+    case "0":
+      return "Bloqueado";
+    case "1":
+      return "Activo";
+    case "2":
+      return "Cancelado";
+    case "3":
+      return "Pendiente";
+    case "4":
+      return "Entregado";
+    case "5":
+      return "Pago";
+    default:
+      return "Error";
+  }
+};
+
+/*-- Subclases de los estados --*/
+export const subClase = (valor) => {
+  const dato = valor.toString();
+  switch (dato) {
+    case "0":
+      return "bloqueado";
+    case "1":
+      return "activo";
+    case "2":
+      return "cancelado";
+    case "3":
+      return "pendiente";
+    case "4":
+      return "pago";
+    case "5":
+      return "entregado";
+    default:
+      return "bloqueado";
+  }
+};
+
+/*-- Cerrar alerta --*/
+export const cerrarAlertaAction = (snackbarId) => (
+  <>
+    <AiFillCloseCircle
+      onClick={() => {
+        closeSnackbar(snackbarId);
+      }}
+    ></AiFillCloseCircle>
+  </>
+);
+
+/*-- imprimir --*/
+export const imprimir = () => {
+  window.print();
+};
--- src/backend/swetAlert2.js
+++ src/backend/swetAlert2.js
@@ -1,7 +1,26 @@
-import swal from'sweetalert2';
+import swal from "sweetalert2";
+
+/*-- alerta claseca --*/
+export const alertaClasica = (
+  icono = "success",
+  mensaje = "",
+  tiempo = 3000
+) => {
+  return swal.fire({
+    icon: icono,
+    title: mensaje,
+    timer: tiempo,
+    timerProgressBar: true,
+  });
+};
 
 /*-- alerta pequeña --*/
-export const alertaToast = (icono = "success", mensaje = "", tiempo = 3000, posicion = "top-end") =>{
+export const alertaToast = (
+  icono = "success",
+  mensaje = "",
+  tiempo = 3000,
+  posicion = "top-end"
+) => {
   const Toast = swal.mixin({
     toast: true,
     position: posicion,
@@ -9,32 +28,61 @@ export const alertaToast = (icono = "success", mensaje = "", tiempo = 3000, posi
     timer: tiempo,
     timerProgressBar: true,
     didOpen: (toast) => {
-      toast.addEventListener('mouseenter', swal.stopTimer)
-      toast.addEventListener('mouseleave', swal.resumeTimer)
-    }
-  })
-  
+      toast.addEventListener("mouseenter", swal.stopTimer);
+      toast.addEventListener("mouseleave", swal.resumeTimer);
+    },
+  });
+
   Toast.fire({
     icon: icono,
-    title: mensaje
-  })
-}
+    title: mensaje,
+  });
+};
 
 /*-- alerta confirmar --*/
-export const alertaConfirmar = async (titulo = "", texto = "", icono = "warning") =>{
-	return swal.fire({
-		title: titulo,
-		text: texto,
-		icon: icono,
-		showCancelButton: true,
-		confirmButtonColor: '#3085d6',
-		cancelButtonColor: '#d33',
-		confirmButtonText: 'Confirmar',
-		cancelButtonText: 'Cancelar',
-	}).then((result) => {
-		if (result.isConfirmed) {
-			return true;
-		}
-		return false;
-	})
-}
+export const alertaConfirmar = async (
+  titulo = "",
+  texto = "",
+  icono = "warning"
+) => {
+  return swal
+    .fire({
+      title: titulo,
+      text: texto,
+      icon: icono,
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Confirmar",
+      cancelButtonText: "Cancelar",
+    })
+    .then((result) => {
+      if (result.isConfirmed) {
+        return true;
+      }
+      return false;
+    });
+};
+
+/*-- alerta campo de texto --*/
+export const alertaInput = async (titulo = "Ingrese el nuevo valor") => {
+  const { value: text } = await swal.fire({
+    title: titulo,
+    input: "text",
+    inputLabel: "",
+    inputPlaceholder: "Escribe aqui",
+  });
+  if (text) {
+    return text;
+  }
+};
+
+/*-- Mostrar Imagen en gran escala --*/
+export const maximixarImagen = (url, nombre) => {
+  swal.fire({
+    title: `${nombre}`,
+    imageUrl: `${url}`,
+    imageWidth: 400,
+    imageHeight: 200,
+  });
+};
