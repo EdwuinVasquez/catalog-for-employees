1c8f284955473e293a1718f68728d5a439c5505f
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-08-08 12:41:37 -0500

    cabezeraMenu.
    Muestra el logo y nombre del usuario.

--- src/components/view/menus/components/cabezeraMenu.jsx
+++ src/components/view/menus/components/cabezeraMenu.jsx
@@ -1,12 +1,17 @@
-import { React } from "react";
-import { RiCloseCircleFill } from "react-icons/ri";
+/*-- Estilos --*/
 import "../../../style/menus/components/cabezeraMenu.css";
+
+/*-- Librerias --*/
+import { React } from "react";
 import { Logo } from "../../logos/logo";
+
+/*-- clases y controladores --*/
 import { useDataContex } from "../../contex";
 
-export function CabezeraMenu({manejarClick}) {		
+export function CabezeraMenu() {
 	const { contexUsuarioLogin } = useDataContex();
-  return(
+
+	return (
 		<div className="cabezeraMenu">
 			<Logo></Logo>
 			<h1 className="cabezeraMenu__nombre" >{contexUsuarioLogin['nombre']}</h1>
