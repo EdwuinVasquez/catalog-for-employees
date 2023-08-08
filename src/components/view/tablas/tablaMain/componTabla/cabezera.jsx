ee68e359e4f1ce8d835c08ece52fc2c19ce39421
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-08-08 13:11:54 -0500

    Encabezado de la tabla.
    Este componente muestra la información como última fecha de modificación,
    de que se trata, buscador y un posible exportador.
    
    props:
    - buscador: nombre de lo que puede ser buscado en el buscador.
    
    - titulo: que es lo que está en la tabla.
    
    - manejarClick: que proceso se debe activar al activar el buscador.
    
    - oculto: es un booleano para Mostar los botones de exportación.
    
    - header: cabeceras de la exportación en Excel.
    
    - body: contenido de la exportación en Excel.
    
    - nombre: nombre que tendrá el archivo por defecto.
    
    - bodyCsv: datos de la exportación en formato CSV.
    
    - bodyJson: datos de la exportación en formato JSON.

--- src/components/view/tablas/tablaMain/componTabla/cabezera.jsx
+++ src/components/view/tablas/tablaMain/componTabla/cabezera.jsx
@@ -9,16 +9,16 @@ import { TablaBuscador } from "./buscador";
 import { TablaExportar } from "./exportar";
 import { FechaActual } from "../../../titulos/fechaActual";
 
-export function TablaCabezera({buscador, titulo, manejarClick, oculto, header, body, bodyCsv, nombre, bodyJson}) {
-  return(
+export function TablaCabezera({ buscador, titulo, manejarClick, oculto, header, body, bodyCsv, nombre, bodyJson }) {
+  return (
     <>
       <section className="tablaMain__cabezera">
         <h2>{titulo}
           <FechaActual></FechaActual>
-         </h2>
+        </h2>
         <TablaBuscador placeholder={buscador} manejarClick={manejarClick}></TablaBuscador>
         <TablaExportar bodyCsv={bodyCsv} bodyJson={bodyJson} nombre={nombre} header={header} body={body} oculto={oculto}></TablaExportar>
       </section>
     </>
-	);
+  );
 };
