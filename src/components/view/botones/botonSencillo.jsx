bc016ade18fe23548c20b0c2ff5ba551b8ce2c67
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-08-08 11:03:17 -0500

    Botón sencillo.
    Este componente renderiza un botón, activa una proceso.
    
    props:
    - texto: contenido que lleva el botón en la pantalla.
    
    - manejarClick: proceso a realizar al identificar un clic sobre él
    Botón.

--- src/components/view/botones/botonSencillo.jsx
+++ src/components/view/botones/botonSencillo.jsx
@@ -1,13 +1,15 @@
-//importacion de librerias
-import {React } from "react";
+/*-- Estilos --*/
 import '../../style/botones/botonSencillo.css';
 
-export function BotonSencillo({texto, manejarClik}) {
-	return(
+/*-- Librerias --*/
+import { React } from "react";
+
+export function BotonSencillo({ texto, manejarClik }) {
+	return (
 		<>
 			<button className="botonSencillo" title="" type="submit" onClick={(e) => manejarClik(e)}>
 				<span>{texto}</span>
-		  </button>
+			</button>
 		</>
 	);
 };
