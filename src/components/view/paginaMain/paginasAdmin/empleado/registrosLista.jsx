a9ef1a49822a108c91f53819f602842bd49d8543
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-08-08 17:01:00 -0500

    Lista de empleados no verificados.
    Muestra una lista con los empleados no verificados, y permite
    verificarlos.
    
    Funciones:
    - generarLista: obtiene la lista de los empleados no verificados y los
    configura en los distintos estados.
    
    - modificarEstadoEmpleado: modifica el estado de verificado de un
    empleado de no verificado a verificado.

--- src/components/view/paginaMain/paginasAdmin/empleado/registrosLista.jsx
+++ src/components/view/paginaMain/paginasAdmin/empleado/registrosLista.jsx
@@ -9,126 +9,123 @@ import { admin } from "../../../../../backend/class/admin.js"
 import { alertaConfirmar, alertaToast } from "../../../../../backend/swetAlert2.js";
 const classAdmin = new admin();
 
+/*-- Titulos de la tabla --*/
+const titulosTabla = ["Cedula", "Nombre", "Registro", "Contato", "Estado", "Verificar",]
+
 export function RegistroLista() {
 	/*-- Lista de nuevos registros --*/
 	const [datosMain, setDatosMain] = useState();
-	
-	/*-- Titulos de la tabla --*/
-	const titulosTabla = ["Cedula", "Nombre", "Registro", "Contato",  "Estado", "Verificar",]
 
 	/*-- Generar la lista de nuevos registros --*/
 	const generarLista = async () => {
 		classAdmin.usuarios()
-		.then(resultado => {
-			if(resultado === false){
-				console.clear();
-				throw new Error("No hay resultados almacenados");
-			}
-			const empleadosLista = resultado.filter(tupla => tupla["ROL"] === "EMPLEADO" && tupla["USUARIO_VERIFICADO"] === "0");
-			const nuevosDatos = empleadosLista.map((valor) =>
-				[{
-					key: valor["CEDULA"],
-					id: valor["CEDULA"],
-					tipo: "normal",
-					valor: valor["CEDULA"],
-					img: "",
-					subClase: "",
-					operacion: "",
-					parametro: ""
-				},{
-					key: valor["CEDULA"],
-					id: valor["CEDULA"],
-					tipo: "normal",
-					valor: valor["NOMBRE"],
-					img: "",
-					subClase: "",
-					operacion: "",
-					parametro: ""
-				},{
-					key: valor["CEDULA"],
-					id: valor["CEDULA"],
-					tipo: "normal",
-					valor: valor["FECHA_REGISTRO"],
-					img: "",
-					subClase: "",
-					operacion: "",
-					parametro: ""
-				},{
-					key: valor["CEDULA"],
-					id: valor["CEDULA"],
-					tipo: "normal",
-					valor: valor["CONTACTO"],
-					img: "",
-					subClase: "",
-					operacion: "",
-					parametro: ""
-				},{
-					key: valor["CEDULA"],
-					id: valor["CEDULA"],
-					tipo: "estado",
-					valor: "3",
-					img: "",
-					subClase: "3",
-					operacion: "",
-					parametro: ""
-				},{
-					key: valor["CEDULA"],
-					id: valor["CEDULA"],
-					tipo: "boton",
-					valor: "Activar centa",
-					img: "",
-					subClase: "BsFillPersonCheckFill",
-					operacion: modificarEstadoEmpleado,
-					parametro: valor["USUARIO_ACTIVADO"] == 1 ? 0 : 1
-				}]
-			);
-			if(nuevosDatos.length > 0) setDatosMain(nuevosDatos);
-			if(nuevosDatos.length <= 0) setDatosMain([[]]);
-		}).catch(function (error) {
-			setDatosMain([[]]);
-			console.error(error);
-		});
+			.then(resultado => {
+				if (resultado === false) {
+					throw new Error("No hay resultados almacenados");
+				}
+				const empleadosLista = resultado.filter(tupla => tupla["ROL"] === "EMPLEADO" && tupla["USUARIO_VERIFICADO"] === "0");
+				const nuevosDatos = empleadosLista.map((valor) =>
+					[{
+						key: valor["CEDULA"],
+						id: valor["CEDULA"],
+						tipo: "normal",
+						valor: valor["CEDULA"],
+						img: "",
+						subClase: "",
+						operacion: "",
+						parametro: ""
+					}, {
+						key: valor["CEDULA"],
+						id: valor["CEDULA"],
+						tipo: "normal",
+						valor: valor["NOMBRE"],
+						img: "",
+						subClase: "",
+						operacion: "",
+						parametro: ""
+					}, {
+						key: valor["CEDULA"],
+						id: valor["CEDULA"],
+						tipo: "normal",
+						valor: valor["FECHA_REGISTRO"],
+						img: "",
+						subClase: "",
+						operacion: "",
+						parametro: ""
+					}, {
+						key: valor["CEDULA"],
+						id: valor["CEDULA"],
+						tipo: "normal",
+						valor: valor["CONTACTO"],
+						img: "",
+						subClase: "",
+						operacion: "",
+						parametro: ""
+					}, {
+						key: valor["CEDULA"],
+						id: valor["CEDULA"],
+						tipo: "estado",
+						valor: "3",
+						img: "",
+						subClase: "3",
+						operacion: "",
+						parametro: ""
+					}, {
+						key: valor["CEDULA"],
+						id: valor["CEDULA"],
+						tipo: "boton",
+						valor: "Activar",
+						img: "",
+						subClase: "usuarioCheck",
+						operacion: modificarEstadoEmpleado,
+						parametro: valor["USUARIO_ACTIVADO"] == 1 ? 0 : 1
+					}]
+				);
+				if (nuevosDatos.length > 0) setDatosMain(nuevosDatos);
+				if (nuevosDatos.length <= 0) setDatosMain([[]]);
+			}).catch(function (error) {
+				setDatosMain([[]]);
+			});
 	}
 
 	/*-- Activar el empleado --*/
-	const modificarEstadoEmpleado = async (cedula, dato) =>{
+	const modificarEstadoEmpleado = async (cedula, dato) => {
 		const nuevoEstado = {
 			cedula: cedula,
 			estado: dato,
 		}
 
 		alertaConfirmar("Seguro que desea verificar el usuario como empleado", `Cedula: ${cedula}`, "warning")
-		.then(confirm =>{
-			if(confirm){
-				classAdmin.modificarEstadoVerificadoUsuario(nuevoEstado)
-		    .then(resultado =>{
-		    	if(resultado === false){
-		    		console.clear();
-		    		alertaToast("warning", "Ocurrio un error", 5000, "top-end");
-		    		throw new Error("No hay resultados almacenados");
-		    	}
-		    	alertaToast(
-		    		resultado[0]["RESULTADO"].toUpperCase().includes("ERROR") ? "error" : "success", 
-		    		resultado[0]["RESULTADO"], 
-		    		5000, 
-		    		"top-end");
-		    	generarLista();
-		    }).catch(function (error) {
-		    	console.error(error);
-		    });
-			}
-		})
-		
+			.then(confirm => {
+				if (confirm) {
+					classAdmin.modificarEstadoVerificadoUsuario(nuevoEstado)
+						.then(resultado => {
+							if (resultado === false) {
+								alertaToast("warning", "Ocurrió un error", 5000, "top-end");
+								throw new Error("No hay resultados almacenados");
+							}
+							alertaToast(
+								resultado[0]["RESULTADO"].toUpperCase().includes("ERROR") ? "error" : "success",
+								resultado[0]["RESULTADO"],
+								5000,
+								"top-end");
+							generarLista();
+						}).catch(function (error) {
+							return error;
+						});
+				}
+			})
+
 	}
 
-  return(
+	return (
 		<>
-			<TablaMain 
-				buscadorNombre="Cedula o nombre" 
-				buscadorTitulo="Sin verificar"  
+			<TablaMain
+				buscadorNombre="Cedula o nombre"
+				buscadorTitulo="Sin verificar"
 				actualizar={generarLista}
 				tablaTitulos={titulosTabla}
-				tablaContenido={datosMain} 
+				tablaContenido={datosMain}
 				oculto={true}>
 			</TablaMain>
 		</>
