3a8f932adcc3594c65df3dfc86c80795aa5b2e13
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-08-08 16:58:34 -0500

    Lista de empleados verificados, generar token.
    Muestra una lista con los empleados verificados. También permite
    activar un token.
    
    Funciones:
    - generarLista: obtiene la lista de los empleados verificados y los
    configura en los distintos estados.
    
    - modificarEstadoEmpleado: modifica el estado de un empleado de
    bloqueado a activo y activa un token.

--- src/components/view/paginaMain/paginasAdmin/empleado/empleadosListaToken.jsx
+++ src/components/view/paginaMain/paginasAdmin/empleado/empleadosListaToken.jsx
@@ -10,156 +10,155 @@ import { alertaConfirmar, alertaToast } from "../../../../../backend/swetAlert2.
 import { useDataContex } from "../../../contex.jsx";
 const classAdmin = new admin();
 
+/*-- Titulos de la tabla --*/
+const titulosTabla = ["Cedula", "Nombre", "N°compras", "Ingreso", "Contato", "Estado", "Token"]
+
 export function EmpleadosListaToken() {
+	const { setCarrito } = useDataContex()
 	const { urlBase } = useDataContex();
 
 	/*-- Lista de empleados --*/
 	const [datosMain, setDatosMain] = useState();
-	
-	/*-- Titulos de la tabla --*/
-	const titulosTabla = ["Cedula", "Nombre", "N°compras", "Ingreso", "Contato", "Estado", "Token"]
 
 	/*-- Generar Lista de empleados --*/
 	const generarLista = async () => {
 		classAdmin.usuarios()
-		.then(resultado => {
-			if(resultado === false){
-				console.clear();
-				throw new Error("No hay resultados almacenados");
-			}
-			const empleadosLista = resultado.filter(tupla => 
-				tupla["ROL"] === "EMPLEADO" && tupla["USUARIO_VERIFICADO"] === "1"
-			);
-			const nuevosDatos = empleadosLista.map((valor) =>
-				[{
-					key: valor["CEDULA"],
-					id: valor["CEDULA"],
-					tipo: "normal",
-					valor: valor["CEDULA"],
-					img: "",
-					subClase: "",
-					operacion: "",
-					parametro: ""
-				},{
-					key: valor["CEDULA"],
-					id: valor["CEDULA"],
-					tipo: "normal",
-					valor: valor["NOMBRE"],
-					img: "",
-					subClase: "",
-					operacion: "",
-					parametro: ""
-				},{
-					key: valor["CEDULA"],
-					id: valor["CEDULA"],
-					tipo: "normal",
-					valor: valor["NUMERO_COMPRAS"],
-					img: "",
-					subClase: "",
-					operacion: "",
-					parametro: ""
-				},{
-					key: valor["CEDULA"],
-					id: valor["CEDULA"],
-					tipo: "normal",
-					valor: valor["ULTIMO_INGRESO"],
-					img: "",
-					subClase: "",
-					operacion: "",
-					parametro: ""
-				},{
-					key: valor["CEDULA"],
-					id: valor["CEDULA"],
-					tipo: "normal",
-					valor: valor["CONTACTO"],
-					img: "",
-					subClase: "",
-					operacion: "",
-					parametro: ""
-				},{
-					key: valor["CEDULA"],
-					id: valor["CEDULA"],
-					tipo: "estado",
-					valor: valor["USUARIO_ACTIVADO"],
-					img: "",
-					subClase: valor["USUARIO_ACTIVADO"],
-					operacion: "",
-					parametro: ""
-				},{
-					key: valor["CEDULA"],
-					id: valor["CEDULA"],
-					tipo: "boton",
-					valor: "Activar token",
-					img: "",
-					subClase: "RiLoginCircleFill",
-					operacion: modificarEstadoEmpleado,
-					parametro: valor["USUARIO_ACTIVADO"] == 1 ? 0 : 1
-				}]
-			);
-			if(nuevosDatos.length > 0) setDatosMain(nuevosDatos);
-			if(nuevosDatos.length <= 0) setDatosMain([[]]);
-		}).catch(function (error) {
-			setDatosMain([[]]);
-			console.error(error);
-		});
+			.then(resultado => {
+				if (resultado === false) {
+					throw new Error("No hay resultados almacenados");
+				}
+				const empleadosLista = resultado.filter(tupla =>
+					tupla["ROL"] === "EMPLEADO" && tupla["USUARIO_VERIFICADO"] === "1"
+				);
+				const nuevosDatos = empleadosLista.map((valor) =>
+					[{
+						key: valor["CEDULA"],
+						id: valor["CEDULA"],
+						tipo: "normal",
+						valor: valor["CEDULA"],
+						img: "",
+						subClase: "",
+						operacion: "",
+						parametro: ""
+					}, {
+						key: valor["CEDULA"],
+						id: valor["CEDULA"],
+						tipo: "normal",
+						valor: valor["NOMBRE"],
+						img: "",
+						subClase: "",
+						operacion: "",
+						parametro: ""
+					}, {
+						key: valor["CEDULA"],
+						id: valor["CEDULA"],
+						tipo: "normal",
+						valor: valor["NUMERO_COMPRAS"],
+						img: "",
+						subClase: "",
+						operacion: "",
+						parametro: ""
+					}, {
+						key: valor["CEDULA"],
+						id: valor["CEDULA"],
+						tipo: "normal",
+						valor: valor["ULTIMO_INGRESO"],
+						img: "",
+						subClase: "",
+						operacion: "",
+						parametro: ""
+					}, {
+						key: valor["CEDULA"],
+						id: valor["CEDULA"],
+						tipo: "normal",
+						valor: valor["CONTACTO"],
+						img: "",
+						subClase: "",
+						operacion: "",
+						parametro: ""
+					}, {
+						key: valor["CEDULA"],
+						id: valor["CEDULA"],
+						tipo: "estado",
+						valor: valor["USUARIO_ACTIVADO"],
+						img: "",
+						subClase: valor["USUARIO_ACTIVADO"],
+						operacion: "",
+						parametro: ""
+					}, {
+						key: valor["CEDULA"],
+						id: valor["CEDULA"],
+						tipo: "boton",
+						valor: "Activar token",
+						img: "",
+						subClase: "login",
+						operacion: modificarEstadoEmpleado,
+						parametro: valor["USUARIO_ACTIVADO"] == 1 ? 0 : 1
+					}]
+				);
+				if (nuevosDatos.length > 0) setDatosMain(nuevosDatos);
+				if (nuevosDatos.length <= 0) setDatosMain([[]]);
+			}).catch(function (error) {
+				setDatosMain([[]]);
+			});
 	}
 
-	/*-- Modificar el estado del empleado --*/
-	const modificarEstadoEmpleado = async (cedula, dato) =>{
+	/*-- Modificar el estado del empleado y activa el token--*/
+	const modificarEstadoEmpleado = async (cedula, dato) => {
 		const nuevoEstado = {
 			cedula: cedula,
 			estado: 1,
 		}
+		localStorage.removeItem("carritoCopia");
+		setCarrito([]);
 
 		alertaConfirmar(
-			"Segur@ que deseas activar el Token", 
-			`Ingresaras en la cuenta del empleado, si el empleado esta bloqueado sera activado y tu cuenta de administrador sera cerrada`, 
+			"Segur@ que deseas activar el Token",
+			`Ingresarás en la cuenta del empleado, si el empleado está bloqueado será activado y tu cuenta de administrador será cerrada`,
 			"warning")
-		.then(confirm =>{
+			.then(confirm => {
 
-			if(confirm){
-				classAdmin.modificarEstadoUsuario(nuevoEstado)
-				.then(resultado =>{
-					if(resultado === false){
-						console.clear();
-						alertaToast("warning", "Ocurrio un error", 5000, "top-end");
-						throw new Error("No hay resultados almacenados");
-					}
-					classAdmin.usuarios(cedula)
-		      .then(resultado =>{
-		      	if(resultado[0]['CEDULA'] == undefined){
-							alertaToast("warning", "Ocurrio un error", 5000, "top-end");
-							throw new Error(resultado);
-		      	}	
-		      	let sesion = {
-		      		cedula: resultado[0]['CEDULA'],
-		      		clave: resultado[0]['CLAVE'],
-		      		nombre: resultado[0]['NOMBRE'],
-		      		rol: resultado[0]['ROL'],
-		      		conctacto: resultado[0]['CONTACTO']
-		      	};
-		      	localStorage.removeItem("usuario");
-		      	localStorage.setItem("usuario", JSON.stringify(sesion));
-		      	window.location.href = `${urlBase}process`;
-		      }).catch(function (error) {
-						console.error(error);
-					});
-				}).catch(function (error) {
-					console.error(error);
-				});
-			}
-			console.log(confirm)
-		}).catch(function (error) {
-			console.error(error);
-		});
-		
+				if (confirm) {
+					classAdmin.modificarEstadoUsuario(nuevoEstado)
+						.then(resultado => {
+							if (resultado === false) {
+								alertaToast("warning", "Ocurrió un error", 5000, "top-end");
+								throw new Error("No hay resultados almacenados");
+							}
+							classAdmin.usuarios(cedula)
+								.then(resultado => {
+									if (resultado[0]['CEDULA'] == undefined) {
+										alertaToast("warning", "Ocurrió un error", 5000, "top-end");
+										throw new Error(resultado);
+									}
+									let sesion = {
+										cedula: resultado[0]['CEDULA'],
+										clave: resultado[0]['CLAVE'],
+										nombre: resultado[0]['NOMBRE'],
+										rol: resultado[0]['ROL'],
+										conctacto: resultado[0]['CONTACTO']
+									};
+									localStorage.removeItem("usuario");
+									localStorage.setItem("usuario", JSON.stringify(sesion));
+									window.location.href = `${urlBase}process`;
+								}).catch(function (error) {
+									return error;
+								});
+						}).catch(function (error) {
+							return error;
+						});
+				}
+			}).catch(function (error) {
+				return error;
+			});
+
 	}
 
-  return(
+	return (
 		<>
-			<TablaMain 
-				buscadorNombre="Cedula o nombre" 
-				buscadorTitulo="Empleados Token"  
+			<TablaMain
+				buscadorNombre="Cedula o nombre"
+				buscadorTitulo="Empleados Token"
 				actualizar={generarLista}
 				tablaTitulos={titulosTabla}
 				tablaContenido={datosMain}
