28ea2fa6128206e8b1864b6e50c72ab0ccea1a74
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-08-08 12:20:43 -0500

    Formulario de login.
    Despliega un formulario de login. Este formulario sirve para ingresar
    al sistema.
    
    Funciones:
    - actualizarDatos: actualiza los datos de un input en el estado,
    recibe el nuevo dado y el key o id del input desde el cual recibe la
    información.
    
    - iniciarSesion: envía los datos en su estado a la API para solicitar
    el ingreso a la cuenta.
    
    - loginPregunta: muestra la pregunta en una alerta.
    
    - loginSinClave: solicita la respuesta a una pregunta, si responde
    correctamente, permite el ingreso a la cuenta de lo contrario muestra
    un error.

--- src/components/view/forms/inicioSesionForm.jsx
+++ src/components/view/forms/inicioSesionForm.jsx
@@ -1,21 +1,29 @@
-//importacion de librerias
-import { React, useState } from "react";
-import { Route, Routes, Link, Navigate } from "react-router-dom";
+/*-- Estilos --*/
+import '../../style/forms/inicioSesionForm.css';
+
+/*-- Librerias --*/
+import { React, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+/*-- Componentes --*/
 import { BotonSencillo } from "../botones/botonSencillo.jsx";
 import { BotonLink } from "../botones/botonLink.jsx";
 import { Logo } from "../logos/logo.jsx";
 import { TituloCentradolo } from "../titulos/tituloCentrado.jsx";
 import { SubtituloCentrado } from "../subtitulos/subtituloCentrado.jsx";
 import { CampoIcono } from "../inputs/inputIcono.jsx";
+
+/*-- clases y controladores --*/
 import { usuario } from "../../../backend/class/usuario.js";
-import '../../style/forms/inicioSesionForm.css'
-import { RegistroFomulario } from "./registroForm.jsx";
 import { useDataContex } from "../contex.jsx";
+import { alertaInput, alertaToast } from "../../../backend/swetAlert2.js"
 const classUsuario = new usuario();
 
 export function LoginFomulario() {
+	/*-- Ruta de la ubicacion de las imagenes en el servidor  --*/
 	const { urlBase } = useDataContex();
 
+	/*-- Valores de los datos en el formulario --*/
 	const [datos, setDatos] = useState(
 		{
 			cedula: "",
@@ -23,60 +31,103 @@ export function LoginFomulario() {
 		}
 	);
 
-	const actualizarDatos = (dato, key) =>{
+	/*-- actualizar el estado con el nuevo valor ingresado en el formulario --*/
+	const actualizarDatos = (dato, key) => {
 		setDatos(() => {
 			let nuevosDatos = datos;
 			nuevosDatos[key] = dato;
 			return nuevosDatos;
 		});
-  }
-
-	const ingresar = (e) =>{
-		e.preventDefault();
-		classUsuario.ingresar(datos['cedula'], datos['clave'])
-		.then(resultado =>{
-			if(resultado[0]['CEDULA'] == undefined){
-				throw new Error(resultado);
-			}	
-			let sesion = {
-				cedula: resultado[0]['CEDULA'],
-				clave: resultado[0]['CLAVE'],
-				nombre: resultado[0]['NOMBRE'],
-				rol: resultado[0]['ROL'],
-				conctacto: resultado[0]['CONTACTO']
-			};
-			localStorage.removeItem("usuario");
-			localStorage.setItem("usuario", JSON.stringify(sesion));
-			window.location.href = `${urlBase}process`;
-		})
-		.catch(function (Error) {
-      console.error(Error);
-    });
 	}
 
-  return(
-		<>
-		<div className="contenedor--centrado">
-			<form className="loginFomulario">
-				<Logo></Logo>
-				<TituloCentradolo texto=""></TituloCentradolo>
-				<SubtituloCentrado 
-					texto="Bienvenido al catalogo de los empleados, antes de empezar ingrese a su cuenta"
-					></SubtituloCentrado>
-				<CampoIcono id="cedula" titulo="Cedula" icono="AiOutlineUser" tipo="number" manejarCambio={actualizarDatos}></CampoIcono>
-				<CampoIcono id="clave" titulo="Clave" icono="GoLock" tipo="password" manejarCambio={actualizarDatos}></CampoIcono>
-				<BotonLink texto="Olvido su contraseña?"></BotonLink>
-				<BotonSencillo texto="Ingresar" manejarClik={ingresar}></BotonSencillo>
-				<Link to="/registro">
-					<BotonSencillo texto="Registrarse" manejarClik={() => {console.log("a")}}></BotonSencillo>
-				</Link>
-			</form>
-		</div>
+	/*-- Envia los datos a la api para su procesamiento  --*/
+	const iniciarSesion = (e) => {
+		e.preventDefault();
+		classUsuario.ingresar(datos['cedula'], datos['clave'])
+			.then(resultado => {
+				if (resultado.name !== undefined) {
+					throw new Error(`${resultado.message}`);
+				}
+				let sesion = {
+					cedula: resultado[0]['CEDULA'],
+					clave: resultado[0]['CLAVE'],
+					nombre: resultado[0]['NOMBRE'],
+					rol: resultado[0]['ROL'],
+					conctacto: resultado[0]['CONTACTO']
+				};
+				localStorage.removeItem("usuario");
+				localStorage.setItem("usuario", JSON.stringify(sesion));
+				window.location.href = `${urlBase}process`;
+			})
+			.catch(function (Error) {
+				alertaToast("error",
+					`${Error.message}`,
+					5000,
+					"top-end");
+			});
+	}
 
-		<Routes>
-    	<Route path="/registro/*" element={<RegistroFomulario />} />
-			<Route path="*" element={null}/>
-    </Routes>
+	useEffect(() => {
+		localStorage.removeItem("carritoCopia");
+	});
+
+	const loginPregunta = (() => {
+		alertaInput("Ingrese su número de cédula")
+			.then(resultado => {
+				if (resultado == undefined || resultado == "") throw new Error("No se ingreso un valor valido")
+				const nuevosDatos = {
+					cedula: resultado,
+				}
+				loginSinClave(nuevosDatos);
+			}).catch((error) => {
+				console.log(error)
+			})
+	});
+
+	const loginSinClave = ((datos) => {
+		classUsuario.ingresarConPregunta(datos['cedula'])
+			.then(resultado => {
+				console.log(resultado)
+				if (resultado.name !== undefined) {
+					throw new Error(`${resultado.message}`);
+				}
+				let sesion = {
+					cedula: resultado[0]['CEDULA'],
+					clave: resultado[0]['CLAVE'],
+					nombre: resultado[0]['NOMBRE'],
+					rol: resultado[0]['ROL'],
+					conctacto: resultado[0]['CONTACTO']
+				};
+				localStorage.removeItem("usuario");
+				localStorage.setItem("usuario", JSON.stringify(sesion));
+				window.location.href = `${urlBase}process`;
+			})
+			.catch(function (Error) {
+				alertaToast("error",
+					`${Error.message}`,
+					5000,
+					"top-end");
+			});
+	})
+
+	return (
+		<>
+			<div className="contenedor--centrado">
+				<form className="loginFomulario">
+					<Logo></Logo>
+					<TituloCentradolo texto=""></TituloCentradolo>
+					<SubtituloCentrado
+						texto="Bienvenido al catalogo de los empleados, antes de empezar ingrese a su cuenta"
+					></SubtituloCentrado>
+					<CampoIcono id="cedula" titulo="Cedula" icono="AiOutlineUser" tipo="number" manejarCambio={actualizarDatos}></CampoIcono>
+					<CampoIcono id="clave" titulo="Clave" icono="GoLock" tipo="password" manejarCambio={actualizarDatos}></CampoIcono>
+					<BotonLink manejarClick={loginPregunta} texto="Olvido su contraseña?"></BotonLink>
+					<BotonSencillo texto="Ingresar" manejarClik={iniciarSesion}></BotonSencillo>
+					<Link to="/registro">
+						<BotonSencillo texto="Registrarse" manejarClik={() => { console.log("a") }}></BotonSencillo>
+					</Link>
+				</form>
+			</div>
 		</>
 	);
 };
