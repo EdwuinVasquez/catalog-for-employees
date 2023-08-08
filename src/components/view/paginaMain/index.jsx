a20ec94d2a1a711925220ba43962fc47500b70a3
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-08-08 16:39:58 -0500

    Index de la aplicacion.

--- src/components/view/paginaMain/index.jsx
+++ src/components/view/paginaMain/index.jsx
@@ -1,31 +1,36 @@
-//importacion de librerias
+/*-- Estilos --*/
+import '../../style/forms/inicioSesionForm.css';
+
+/*-- Librerias --*/
 import { React, useEffect } from "react";
-import { Route, Routes, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+/*-- Componentes --*/
 import { BotonSencillo } from "../botones/botonSencillo.jsx";
 import { Logo } from "../logos/logo.jsx";
 import { TituloCentradolo } from "../titulos/tituloCentrado.jsx";
 import { SubtituloCentrado } from "../subtitulos/subtituloCentrado.jsx";
-import '../../style/forms/inicioSesionForm.css'
 
 export function Index() {
-	useEffect (() =>{
+	useEffect(() => {
 		localStorage.removeItem("usuario");
-	})
-  return(
-	<div className="contenedor--centrado">
-		<form className="loginFomulario">
-			<Logo></Logo>
-			<TituloCentradolo texto="Rimoplasticas S.A"></TituloCentradolo>
-			<SubtituloCentrado 
-				texto="Bienvenido al catalogo de los empleados, antes de empezar ingrese a su cuenta"
+		localStorage.removeItem("carritoCopia");
+	});
+	return (
+		<div className="contenedor--centrado">
+			<form className="loginFomulario">
+				<Logo></Logo>
+				<TituloCentradolo texto="Rimoplasticas S.A"></TituloCentradolo>
+				<SubtituloCentrado
+					texto="Bienvenido al catalogo de los empleados, antes de empezar ingrese a su cuenta"
 				></SubtituloCentrado>
-			<Link to="/login">
-				<BotonSencillo texto="Ingresar" manejarClik={() => {console.log("b")}}></BotonSencillo>
-			</Link>
-			<Link to="/registro">
-				<BotonSencillo texto="Registrarse" manejarClik={() => {console.log("a")}}></BotonSencillo>
-			</Link>
-		</form>
-	</div>
+				<Link to="/login">
+					<BotonSencillo texto="Ingresar" manejarClik={() => { console.log() }}></BotonSencillo>
+				</Link>
+				<Link to="/registro">
+					<BotonSencillo texto="Registrarse" manejarClik={() => { console.log() }}></BotonSencillo>
+				</Link>
+			</form>
+		</div>
 	);
 };
