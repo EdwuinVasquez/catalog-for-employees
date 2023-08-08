af90b37c555bc01042a2e1b9c14057d1a15224a1
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-08-08 15:14:17 -0500

    Datos de la tarjeta de producto normal.
    Este componente retorna datos como valor total de la compra y un botón
    para Mostar los detalles del producto.
    
    props:
    - precio: el valor de venta del producto.
    
    - id: el código o subclase del producto en cuestión.
    
    - buscarProducto: proceso el cual permite realizar una búsqueda
    específica de todos los estilos vinculados a este producto.
    
    - carrito: es un estado que contienen si el carrito está activo o no.
    
    - activarDetalles: activa el visor de los estilos del producto.

--- src/components/view/cards/cardProducto/components/finalCard.jsx
+++ src/components/view/cards/cardProducto/components/finalCard.jsx
@@ -7,19 +7,12 @@ import { React } from "react";
 import { MdOutlineShoppingCart } from "react-icons/md";
 
 /*-- Clases y controladores --*/
-import { regex } from '../../../../../backend/regex';
 import { useDataContex } from '../../../contex';
+import { formatearNumero } from '../../../../../backend/funcioneGenerales';
 
-export function FinalCardProducto({precio, id , buscarProducto, carrito, activarDetalles}) {
+export function FinalCardProducto({ precio, id, buscarProducto, carrito, activarDetalles }) {
 	/*-- Estado global menu --*/
-	const {setContexMenu } = useDataContex();
-
-	/*-- Formatear numero a pesos --*/
-	const formatearNumero = (numero) =>{
-		const expresion = regex.pesos;
-		const remplazo = "$1.";
-		return numero.toString().replace(expresion, remplazo);
-	}
+	const { setContexMenu } = useDataContex();
 
 	/*-- Cerrar los detalles --*/
 	const procesarClick = ((codigo) => {
@@ -29,11 +22,11 @@ export function FinalCardProducto({precio, id , buscarProducto, carrito, activar
 		buscarProducto(codigo);
 	})
 
-	return(
+	return (
 		<div className="cardProducto__final">
-  			<span className="cardProducto__final--titulo">${formatearNumero(precio)}</span>
-  			<div className="cardProducto__final--boton"> 
-			 	<MdOutlineShoppingCart className="cardProducto__final--svg" onClick={() => procesarClick(id)} ></MdOutlineShoppingCart>
+			<span className="cardProducto__final--titulo">${formatearNumero(precio)}</span>
+			<div className="cardProducto__final--boton">
+				<MdOutlineShoppingCart className="cardProducto__final--svg" onClick={() => procesarClick(id)} ></MdOutlineShoppingCart>
 			</div>
 		</div>
 	);
