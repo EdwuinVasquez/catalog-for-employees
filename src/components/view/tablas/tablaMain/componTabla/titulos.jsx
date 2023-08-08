b1ac72719cdd4e93572b5c9c6f94ac4603861fce
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-08-08 13:21:13 -0500

    Títulos de la tabla.
    Retorna un tr con los títulos de la tabla.
    
    props:
    - titulos: recibe los títulos que contendrá la tabla.

--- src/components/view/tablas/tablaMain/componTabla/titulos.jsx
+++ src/components/view/tablas/tablaMain/componTabla/titulos.jsx
@@ -1,21 +1,22 @@
 /*-- Estilos --*/
 import "../../../../style/tabla/tablaMain/cabezera.css";
+import { v4 as uuidv4 } from 'uuid';
 
 /*-- Importaciones --*/
 import { React } from "react";
 
-export function TablaTitulos({titulos}) {
-  return(
+export function TablaTitulos({ titulos }) {
+  return (
     <>
-    <thead >
-      <tr>
-	      {
-					titulos.map((titulo) =>
-						<th key={titulo} className="tablaMain__cabezera--titulos"> {titulo} </th>
-					)
-				}
-      </tr>
-    </thead>
+      <thead >
+        <tr>
+          {
+            titulos.map((titulo) =>
+              <th key={uuidv4()} className="tablaMain__cabezera--titulos"> {titulo} </th>
+            )
+          }
+        </tr>
+      </thead>
     </>
-	);
+  );
 };
