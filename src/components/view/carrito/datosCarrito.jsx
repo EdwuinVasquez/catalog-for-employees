15dc9c1454bcaa8263bdc6da21d77d0ed5aec0b2
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-08-08 16:16:10 -0500

    Datos de cada ítem del carrito.
    Muestra los datos de cada ítem cargado previamente en el carrito.
    
    props:
    - nombre: nombre del producto.
    
    - cantidad: cantidad a comprar del ítem.
    
    - valorUnidad: valor de cada unidad del producto.
    
    - imagenItem: imagen de identificación del ítem.
    
    - manejarClick: proceso al darle clic al botón borrar ítem.
    
    - parametros: datos generales del ítem se usa como parametro en caso de
    dar clic sobre el botón eliminar.
    
    Funciones:
    - calcularCosto: Calcula el costo del ítem (Esta función esta
    desactivada, es decir, no se está usando como debería)

---
+++ src/components/view/carrito/datosCarrito.jsx
@@ -0,0 +1,47 @@
+/*-- Estilos --*/
+import '../../style/carrito/datos.css';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+
+/*-- Librerias --*/
+import { React } from "react";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
+/*-- Componentes --*/
+import { GrFormClose } from "react-icons/gr";
+
+/*-- Clases y controladores --*/
+import { useDataContex } from '../contex';
+import { formatearNumero, formatearTexto } from '../../../backend/funcioneGenerales';
+
+export function CarritoCatalogoItems({ nombre, cantidad, valorUnidad, imagenItem, manejarClick, parametros }) {
+	const { urlBaseImg } = useDataContex();
+
+	const calcularCosto = ((cantidad, valor) => {
+		const total = cantidad * valor
+		return formatearNumero(total);
+	});
+
+	return (
+		<div className="carritoCatalogo__item">
+			<div className="carritoCatalogo__imagen">
+				<LazyLoadImage
+					src={`${urlBaseImg}${imagenItem}`}
+					alt=""
+					className='carritoCatalogo__imagen--img'
+					effect="blur" />
+			</div>
+			<div className="carritoCatalogo__datos">
+				<section>
+					<span className='carritoCatalogo__datos--nombre' title={nombre}>{formatearTexto(nombre, 55)}</span>
+				</section>
+				<section>
+					<span className='carritoCatalogo__datos--cantidad'>
+						{cantidad} x
+						<span className='carritoCatalogo__datos--precio'> ${calcularCosto(1, valorUnidad)}</span>
+					</span>
+				</section>
+			</div>
+			<GrFormClose className="carritoCatalogo__icono" onClick={() => manejarClick(parametros)}></GrFormClose>
+		</div>
+	);
+};
