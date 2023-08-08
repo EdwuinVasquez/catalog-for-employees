85dcccf7d9878b95a8051b0e1f7801bdd42f630b
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-08-08 14:04:41 -0500

    Tabla main
    Estaba se usa para Mostar o modificar información de la API o de estados
    globales.
    
    props:
    - buscadorNombre: nombre de la tabla.
    
    - nombre: nombre del archivo a exportar.
    
    - bodyCsv: datos de exportación en formato CSV.
    
    - bodyJson: datos de exportación en formato JSON.
    
    - header: cabeceras de exportación de Excel.
    
    - body: contenido de exportación de Excel.
    
    - buscadorTitulo: nombre de lo que se puede buscar a través del
    buscador.
    
    - tablaTitulos: titulos de la tabla.
    
    - tablaContenido: contenido de la tabla.
    
    - actualizar: proceso para realizar la actualización de los datos de la
    tabla.
    
    - oculto: ocultar o mostrar la exportación.
    
    Funciones:
    - realizarBusqueda: realiza una búsqueda entre los datos de la tabla.

--- src/components/view/tablas/tablaMain/tablaMain.jsx
+++ src/components/view/tablas/tablaMain/tablaMain.jsx
@@ -7,31 +7,36 @@ import { React, useState } from "react";
 /*-- Componentes --*/
 import { TablaCabezera } from "./componTabla/cabezera";
 import { TablaCotenido } from "./componTabla/contenido";
+import { alertaToast } from "../../../../backend/swetAlert2";
 
-export function TablaMain({buscadorNombre, nombre, bodyCsv, bodyJson, header, body, buscadorTitulo, tablaTitulos, tablaContenido, actualizar, oculto}) {
+export function TablaMain({ buscadorNombre, nombre, bodyCsv, bodyJson, header, body, buscadorTitulo, tablaTitulos, tablaContenido, actualizar, oculto }) {
   /*-- Lista de datos filtrados --*/
   const [contenidoFilter, setContenidoFilter] = useState([[]]);
-  
+
   /*-- Buscar en el contenido de la tabla --*/
-  const realizarBusqueda = (buscar) =>{
+  const realizarBusqueda = (buscar) => {
     try {
       let valueBuscador = buscar.trim();
       let datosFiltrados = tablaContenido.filter(
-        tupla => (tupla[0]['valor'].toLowerCase().includes(valueBuscador.toLowerCase()) || tupla[1]['valor'].toLowerCase().includes(valueBuscador.toLowerCase())) 
+        tupla => (tupla[0]['valor'].toLowerCase().includes(valueBuscador.toLowerCase()) || tupla[1]['valor'].toLowerCase().includes(valueBuscador.toLowerCase()))
       );
-      console.clear();
-      if(datosFiltrados.length > 0) setContenidoFilter(datosFiltrados);
+      if (datosFiltrados.length > 0) {
+        alertaToast("success", "Datos filtrados", 6000, "top-end")
+        setContenidoFilter(datosFiltrados);
+      }else{
+        alertaToast("error", "No se encontraron datos", 6000, "top-end")
+      }
     } catch (error) {
-      console.warn(error);
+      return error;
     }
   }
 
-  return(
+  return (
     <>
-    <div className="tablaMain">
-      <TablaCabezera nombre={nombre} bodyCsv={bodyCsv} bodyJson={bodyJson} header={header} body={body} buscador={buscadorNombre} titulo={buscadorTitulo} manejarClick={realizarBusqueda} oculto={oculto}></TablaCabezera>
-      <TablaCotenido titulos={tablaTitulos} actualizar={actualizar} contenido={(contenidoFilter[0].length >= 2 ? contenidoFilter : tablaContenido)}></TablaCotenido>
-    </div>
+      <div className="tablaMain">
+        <TablaCabezera nombre={nombre} bodyCsv={bodyCsv} bodyJson={bodyJson} header={header} body={body} buscador={buscadorNombre} titulo={buscadorTitulo} manejarClick={realizarBusqueda} oculto={oculto}></TablaCabezera>
+        <TablaCotenido titulos={tablaTitulos} actualizar={actualizar} contenido={(contenidoFilter[0].length >= 2 ? contenidoFilter : tablaContenido)}></TablaCotenido>
+      </div>
     </>
-	);
+  );
 };
