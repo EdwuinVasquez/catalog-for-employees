f31d5d2832708b3d278ad1240cdeef52b055e28c
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-08-08 15:58:36 -0500

    Agregar productos al carrito
    Retorna un componente que permite agregar productos al carrito.
    
    props:
    - precio: valor de venta del producto.
    
    - productoSelect: datos del estilo del producto seleccionado.
    
    - precioBase: valor de venta sin aplicar los descuentos.
    
    Funciones:
    - agregarProducto: se ejecuta al presionar clic sobre el botón agregar
    al carrito, esta evalúa si el producto ya está en el carrito y procede a
    modificarlo según sea requerido.
    
    - agregarNuevoProductoCarrito: Agrega un nuevo producto al carrito.
    
    - modificarProductoCarrito: aumenta en 1 la cantidad de un producto.

--- src/components/view/cards/cardProductoDetalles/components/finalCardDetalles.jsx
+++ src/components/view/cards/cardProductoDetalles/components/finalCardDetalles.jsx
@@ -5,62 +5,44 @@ import 'react-lazy-load-image-component/src/effects/blur.css';
 /*-- Librerias --*/
 import { React } from "react";
 import { MdOutlineShoppingCart } from "react-icons/md";
-import { AiFillCloseCircle } from "react-icons/ai";
-import { useSnackbar, closeSnackbar } from 'notistack';
+import { useSnackbar } from 'notistack';
 
 /*-- Clases y controladores --*/
-import { regex } from '../../../../../backend/regex';
 import { useDataContex } from '../../../contex.jsx';
-import { emple } from "../../../../../backend/class/empleado.js"
+import { emple } from "../../../../../backend/class/empleado.js";
+import { cerrarAlertaAction as action, formatearNumero } from "../../../../../backend/funcioneGenerales.js"
 const classEmple = new emple();
 
-export function FinalCardProductoDetalles({precio, productoSelect, precioBase}) {
+export function FinalCardProductoDetalles({ precio, productoSelect, precioBase }) {
 	/*-- Carrito de compras --*/
-	const { carrito, setCarrito } =  useDataContex();
-	
+	const { carrito, setCarrito } = useDataContex();
+
 	/*-- Alerta --*/
 	const { enqueueSnackbar } = useSnackbar();
 
-	/*-- Cerrar alerta --*/
-	const action = (snackbarId) =>(
-		<>
-      <AiFillCloseCircle onClick={() => { closeSnackbar(snackbarId) }}>
-			</AiFillCloseCircle>
-		</>
-	)
-
-	/*-- Formatear Numeros en pesos --*/
-	const formatearNumero = (numero) =>{
-		const expresion = regex.pesos;
-		const remplazo = "$1.";
-		return numero.toString().replace(expresion, remplazo);
-	}
-
 	/*-- Agregar producto al carrito --*/
 	const agregarProducto = (() => {
-		if(productoSelect != 0 &&  productoSelect[0] != undefined){
-			if(carrito.length <= 0) {
+		if (productoSelect != 0 && productoSelect[0] != undefined) {
+			if (carrito.length <= 0) {
 				setCarrito([productoSelect]);
 				classEmple.actualizarCarrito([productoSelect]);
-			} else{
+			} else {
 				let buscarProducto = carrito.some(
 					value => value[0]["nombreItem"] == productoSelect[0]['nombreItem']
 				);
-				if(buscarProducto){
+				if (buscarProducto) {
 					modificarProductoCarrito(productoSelect);
-				}else{
+				} else {
 					agregarNuevoProductoCarrito(productoSelect);
 				}
 			}
 			enqueueSnackbar(`Producto agregado al carrito`, {
 				variant: "success",
-				autoHideDuration: 1500,
 				action
 			})
-		}else{
+		} else {
 			enqueueSnackbar(`No has seleccionado un color`, {
 				variant: "error",
-				autoHideDuration: 1500,
 				action
 			})
 		}
@@ -72,14 +54,14 @@ export function FinalCardProductoDetalles({precio, productoSelect, precioBase}) 
 		setCarrito(nuevoCarrito);
 		classEmple.actualizarCarrito(nuevoCarrito);
 	});
-	
+
 	/*-- Aumentar la cantidad de un item --*/
 	const modificarProductoCarrito = ((producto) => {
 		let nuevoCarrito = carrito.map(item => {
-			if(item[0]["nombreItem"] == producto[0]['nombreItem']){
+			if (item[0]["nombreItem"] == producto[0]['nombreItem']) {
 				item[0]["cantidad"]++;
 				return item;
-			}else{
+			} else {
 				return item;
 			}
 		});
@@ -87,16 +69,15 @@ export function FinalCardProductoDetalles({precio, productoSelect, precioBase}) 
 		classEmple.actualizarCarrito(nuevoCarrito);
 	});
 
-	return(
+	return (
 		<div className="cardProductoDetalles__final">
 			<div className="cardProductoDetalles__final--precios">
 				<span className="cardProductoDetalles__final--precioBase">${formatearNumero(precioBase)}</span>
 				<span className="cardProductoDetalles__final--precioFinal">${formatearNumero(precio)}</span>
 			</div>
-  			<div className="cardProductoDetalles__final--boton" onClick={agregarProducto}> 
-			 	<MdOutlineShoppingCart className="cardProductoDetalles__final--svg" ></MdOutlineShoppingCart>
+			<div className="cardProductoDetalles__final--boton" onClick={agregarProducto}>
+				<MdOutlineShoppingCart className="cardProductoDetalles__final--svg" ></MdOutlineShoppingCart>
 			</div>
 		</div>
 	);
 };
-
