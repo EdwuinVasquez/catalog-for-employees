aba77fd3114642f511af34bbbbfc18ce7a03bf14
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-08-08 11:37:26 -0500

    Input de tipo select.
    Es un input con un listado opciones, este componente se usa cuando las
    opciones del campo ya están limitadas a opciones muy específicas, como
    la pregunta de seguridad, imagen y categoría.
    
    props:
    - id: identificador del input desde el lugar de invocación.
    
    - texto: título de solicitud de lo que se requiere en el input,
    ejemplo: imagen.
    
    - expresion: recibe una expresión regular para evaluar que lo que se
    selecciono con las características requeridas.
    
    - manejarCambio: una función para aplicar los cambios en el componente
    padre.
    
    - listaOpciones: listado de opciones que tendrá el input.
    
    -manejarNull: función a ejecutar en caso de que el props listaOpciones
    sea undefined o null.
    
    Funciones:
    - value: evalúa los cambios realizados son correctos, actualiza el
    componente y envía la información recopilada al componente padre.
    
    - opcion: genera el listado de opciones partir del prop listaOpciones.

--- src/components/view/inputs/selectBase.jsx
+++ src/components/view/inputs/selectBase.jsx
@@ -1,49 +1,51 @@
-//importacion de librerias
-import { React, useState } from "react";
+/*-- Estilo --*/
 import '../../style/inputs/selectBase.css';
 
-export function SelectBase({id, texto, expresion, manejarCambio, listaOpciones, manejarNull}) {
-	const [datosValido, setDatosValido] = useState(false);
-	const [valueCampo, setValueCampo] = useState(true);
+/*-- Librerias --*/
+import { React, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
-  const value = (event) =>{
+export function SelectBase({ id, texto, expresion, manejarCambio, listaOpciones, manejarNull }) {
+  const [datosValido, setDatosValido] = useState(false);
+  const [valueCampo, setValueCampo] = useState(true);
+
+  /*-- Obtener el valor seleccionado --*/
+  const value = (event) => {
     let value = event.target.value;
     setDatosValido(expresion.test(value));
     setValueCampo(value.trim() == "");
     manejarCambio(value.trim(), expresion.test(value), id);
   }
 
-	const opcion = (() =>{
+  /*-- Generar lista de opciones --*/
+  const opcion = (() => {
     try {
-      console.clear();
-      if(listaOpciones[0].length >= 1 || listaOpciones.length >= 1){
+      if (listaOpciones[0].length >= 1 || listaOpciones.length >= 1) {
         return listaOpciones.map((tupla) =>
-					<option key={tupla[0]['key']} defaultValue={tupla[0]['valor']}>{tupla[0]['valor']}</option>
+          <option key={uuidv4()} defaultValue={tupla[0]['valor']}>{tupla[0]['valor']}</option>
         )
-      }else{
-        return <option key="1232" defaultValue="no hay datos">no hay datos</option>
+      } else {
+        return <option key={uuidv4()} defaultValue="no hay datos">no hay datos</option>
       }
     } catch (error) {
       manejarNull();
     }
-
-    
   });
 
-  return(
-		<>
-		<div className="selectBase">
-			<select className="selectBase__select" onChange={value} required="" autoComplete="off">
-				<option selected={true} defaultValue="NAN" disabled="disabled">seleccione una opcion</option>
-				{
-					opcion()
-				}
-			</select>
-			<i className="selectBase__flecha"></i>
-			<label 
-        className={"selectBase__texto  " + (datosValido ? "selectBase__texto--verde" : "selectBase__texto--rojo") + (valueCampo ? "  selectBase__texto--vacio" : "  ")} 
+  return (
+    <>
+      <div className="selectBase">
+        <select className="selectBase__select" onChange={value} required="" autoComplete="off">
+          <option selected={true} defaultValue="NAN" disabled="disabled">seleccione una opcion</option>
+          {
+            opcion()
+          }
+        </select>
+        <i className="selectBase__flecha"></i>
+        <label
+          className={"selectBase__texto  " + (datosValido ? "selectBase__texto--verde" : "selectBase__texto--rojo") + (valueCampo ? "  selectBase__texto--vacio" : "  ")}
         >{texto}</label>
-		</div>
-		</>
-	);
+      </div>
+    </>
+  );
 };
