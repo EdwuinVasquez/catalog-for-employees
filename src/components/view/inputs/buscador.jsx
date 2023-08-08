3fca5d1289b02256379b99007a0e52fefc4c4b58
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-08-08 11:51:51 -0500

    Buscador.
    Retorna un input que funciona como buscador de información.
    
    props:
    - placeholder: texto del fondo del input.
    
    - manejarClick: proceso a ejecutar al activar el buscador.
    
    Funciones:
    - actualizarBuscador: Actualiza el estado del buscador al detectar un
    cambio en el input.
    
    - enviar: envía la información actual del estado del input a la función
    recibida
    
    - pulsar: cada que se presione una tecla evalúa si es la tecla enter,
    en caso de serlo activará la función enviar.

--- src/components/view/inputs/buscador.jsx
+++ src/components/view/inputs/buscador.jsx
@@ -1,30 +1,41 @@
-//importacion de librerias
+/*-- Estilos --*/
+import "../../style/inputs/buscador.css";
+
+/*-- Librerias --*/
 import { React, useState } from "react";
-import "../../style/inputs/buscador.css"
 import { CgSearch } from "react-icons/cg";
 
-export function Buscador({placeholder, manejarClick}) {
+export function Buscador({ placeholder, manejarClick }) {
+  /*-- Texto del buscador --*/
   const [buscar, setBuscar] = useState(null);
 
+  /*-- Actualizar buscador --*/
   const actualizarBuscador = (e) => {
     let value = e.target.value.trim();
     setBuscar(value.toUpperCase())
   }
 
+  /*-- Enviar dato a buscar --*/
   const enviar = (e) => {
-    manejarClick(buscar)
+    const palabra = buscar.trim();
+    if (palabra && palabra.slice(-1).toLowerCase() === 's') {
+      manejarClick(palabra.slice(0, -1));
+    }else{
+      manejarClick(palabra)
+    }
   }
 
+  /*-- Activar al precionar enter --*/
   function pulsar(e) {
     if (e.keyCode === 13 && !e.shiftKey) {
       enviar();
     }
   }
 
-  return(
+  return (
     <div className="buscador">
-        <input className="buscador__input" onKeyDown={pulsar} autoComplete="off" onChange={actualizarBuscador} type="search" placeholder={placeholder} name="text" />
-        <CgSearch className="buscador__icono" onClick={enviar}></CgSearch>
+      <input className="buscador__input" onKeyDown={pulsar} autoComplete="off" onChange={actualizarBuscador} type="search" placeholder={placeholder} name="text" />
+      <CgSearch className="buscador__icono" onClick={enviar}></CgSearch>
     </div>
-	);
+  );
 };
