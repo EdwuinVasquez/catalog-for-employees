0d0fcaea04baa62e7471de68fcce834bf740d907
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-08-08 15:07:27 -0500

    Imagen de tarjetas.
    Retorna una imagen para las diferentes tarjetas.
    
    props:
    -url: la dirección url de la imagen.

--- src/components/view/cards/cardProducto/components/imagen.jsx
+++ src/components/view/cards/cardProducto/components/imagen.jsx
@@ -3,19 +3,20 @@ import '../../../../style/cardProducto/components/imagen.css';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
 /*-- Librerias --*/
-import {	React } from "react";
+import { React } from "react";
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 /*-- Clases y controladores --*/
 import { useDataContex } from "../../../contex.jsx";
 
-export function ImagenCardProducto({url}) {
-	const { urlBaseImg } = useDataContex();
+export function ImagenCardProducto({ url }) {
+	/*-- Url base de la carpeta images --*/
+  const { urlBaseImg } = useDataContex();
 
-	return(
+  return (
     <div className="cardProducto__imagen">
-      <LazyLoadImage className="cardProducto__imagen--imgen" src={`${urlBaseImg}${url}`} alt="" effect="blur"/> 
+      <LazyLoadImage className="cardProducto__imagen--imgen" src={`${urlBaseImg}${url}`} alt="" effect="blur" />
     </div>
-	);
+  );
 };
 
--- src/components/view/cards/cardProductoDetalles/components/imagenDetalles.jsx
+++ src/components/view/cards/cardProductoDetalles/components/imagenDetalles.jsx
@@ -3,20 +3,20 @@ import '../../../../style/cardProductoDetalles/components/imagen.css';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
 /*-- Librerias --*/
-import {	React } from "react";
+import { React } from "react";
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 /*-- Clases y controladores --*/
 import { useDataContex } from "../../../contex.jsx";
 
-export function ImagenCardProductoDetalles({url}) {
+export function ImagenCardProductoDetalles({ url }) {
 	/*-- Url base de la carpeta images --*/
 	const { urlBaseImg } = useDataContex();
 
-	return(
-    <div className="cardProductoDetalles__imagen">
-      <LazyLoadImage className="cardProductoDetalles__imagen--imgen" src={`${urlBaseImg}${url}`} alt="" effect="blur"/> 
-    </div>
+	return (
+		<div className="cardProductoDetalles__imagen">
+			<LazyLoadImage className="cardProductoDetalles__imagen--imgen" src={`${urlBaseImg}${url}`} alt="" effect="blur" />
+		</div>
 	);
 };
 
