e8f111187be6a0bbff17c47b23955addeedeea2e
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-08-08 16:46:48 -0500

    Lista de empleados verificados.
    Muestra una lista con los empleados verificados. La cual permite hacer
    algunas modificaciones a las cuentas.
    
    Funciones:
    - generarLista: obtiene la lista de los empleados verificados y los
    configura en los distintos estados.
    
    - modificarEstadoEmpleado: modifica el estado de un empleado de activo
    ha bloqueado y viceversa.
    
    - eliminarEmpleadoSistema: permite borrar un empleado del sistema desde
    que no posea ventas vinculadas a él.
    
    - modificarClaveEmpleado: permite alterar la clave de un usuario en
    caso de ser necesario

--- src/components/view/paginaMain/paginasAdmin/empleado/empleadosLista.jsx
+++ src/components/view/paginaMain/paginasAdmin/empleado/empleadosLista.jsx
@@ -6,179 +6,214 @@ import { TablaMain } from "../../../tablas/tablaMain/tablaMain.jsx";
 
 /*-- Clases y controladores --*/
 import { admin } from "../../../../../backend/class/admin.js"
-import { alertaConfirmar, alertaToast } from "../../../../../backend/swetAlert2.js";
+import { alertaConfirmar, alertaToast, alertaInput } from "../../../../../backend/swetAlert2.js";
 const classAdmin = new admin();
 
+/*-- Titulos de la tabla --*/
+const titulosTabla = ["Cedula", "Nombre", "N°compras", "Ingreso", "Contato", "Estado", "Cambiar estado", "Cambiar clave", "Eliminar empleado"]
 
 export function EmpleadosLista() {
 	/*-- Lista de empleados --*/
 	const [datosMain, setDatosMain] = useState();
-	
-	/*-- Titulos de la tabla --*/
-	const titulosTabla = ["Cedula", "Nombre", "N°compras", "Ingreso", "Contato", "Estado", "Cambiar estado", "Eliminar empleado"]
 
 	/*-- Generar lista de empleados --*/
 	const generarLista = async () => {
 		classAdmin.usuarios()
-		.then(resultado => {
-			if(resultado === false){
-				console.clear();
-				throw new Error("No hay resultados almacenados");
-			}
-			const empleadosLista = resultado.filter(tupla => 
-				tupla["ROL"] === "EMPLEADO" && tupla["USUARIO_VERIFICADO"] === "1"
-			);
-			const nuevosDatos = empleadosLista.map((valor) =>
-				[{
-					key: valor["CEDULA"],
-					id: valor["CEDULA"],
-					tipo: "normal",
-					valor: valor["CEDULA"],
-					img: "",
-					subClase: "",
-					operacion: "",
-					parametro: ""
-				},{
-					key: valor["CEDULA"],
-					id: valor["CEDULA"],
-					tipo: "normal",
-					valor: valor["NOMBRE"],
-					img: "",
-					subClase: "",
-					operacion: "",
-					parametro: ""
-				},{
-					key: valor["CEDULA"],
-					id: valor["CEDULA"],
-					tipo: "normal",
-					valor: valor["NUMERO_COMPRAS"],
-					img: "",
-					subClase: "",
-					operacion: "",
-					parametro: ""
-				},{
-					key: valor["CEDULA"],
-					id: valor["CEDULA"],
-					tipo: "normal",
-					valor: valor["ULTIMO_INGRESO"],
-					img: "",
-					subClase: "",
-					operacion: "",
-					parametro: ""
-				},{
-					key: valor["CEDULA"],
-					id: valor["CEDULA"],
-					tipo: "normal",
-					valor: valor["CONTACTO"],
-					img: "",
-					subClase: "",
-					operacion: "",
-					parametro: ""
-				},{
-					key: valor["CEDULA"],
-					id: valor["CEDULA"],
-					tipo: "estado",
-					valor: valor["USUARIO_ACTIVADO"],
-					img: "",
-					subClase: valor["USUARIO_ACTIVADO"],
-					operacion: "",
-					parametro: ""
-				},{
-					key: valor["CEDULA"],
-					id: valor["CEDULA"],
-					tipo: "boton",
-					valor: "Modificar estado",
-					img: "",
-					subClase: "RxUpdate",
-					operacion: modificarEstadoEmpleado,
-					parametro: valor["USUARIO_ACTIVADO"] == 1 ? 0 : 1
-				},{
-					key: valor["CEDULA"],
-					id: valor["CEDULA"],
-					tipo: "boton",
-					valor: "Eliminar empleado",
-					img: "",
-					subClase: "MdDelete",
-					operacion: eliminarEstadoEmpleado,
-					parametro: valor["USUARIO_ACTIVADO"] == 1 ? 0 : 1
-				}]
-			);
-			if(nuevosDatos.length > 0) setDatosMain(nuevosDatos);
-			if(nuevosDatos.length <= 0) setDatosMain([[]]);
-		}).catch(function (error) {
-			setDatosMain([[]]);
-			console.error(error);
-		});
+			.then(resultado => {
+				if (resultado === false) {
+					throw new Error("No hay resultados almacenados");
+				}
+				const empleadosLista = resultado.filter(tupla =>
+					tupla["ROL"] === "EMPLEADO" && tupla["USUARIO_VERIFICADO"] === "1"
+				);
+				const nuevosDatos = empleadosLista.map((valor) =>
+					[{
+						key: valor["CEDULA"],
+						id: valor["CEDULA"],
+						tipo: "normal",
+						valor: valor["CEDULA"],
+						img: "",
+						subClase: "",
+						operacion: "",
+						parametro: ""
+					}, {
+						key: valor["CEDULA"],
+						id: valor["CEDULA"],
+						tipo: "normal",
+						valor: valor["NOMBRE"],
+						img: "",
+						subClase: "",
+						operacion: "",
+						parametro: ""
+					}, {
+						key: valor["CEDULA"],
+						id: valor["CEDULA"],
+						tipo: "normal",
+						valor: valor["NUMERO_COMPRAS"],
+						img: "",
+						subClase: "",
+						operacion: "",
+						parametro: ""
+					}, {
+						key: valor["CEDULA"],
+						id: valor["CEDULA"],
+						tipo: "normal",
+						valor: valor["ULTIMO_INGRESO"],
+						img: "",
+						subClase: "",
+						operacion: "",
+						parametro: ""
+					}, {
+						key: valor["CEDULA"],
+						id: valor["CEDULA"],
+						tipo: "normal",
+						valor: valor["CONTACTO"],
+						img: "",
+						subClase: "",
+						operacion: "",
+						parametro: ""
+					}, {
+						key: valor["CEDULA"],
+						id: valor["CEDULA"],
+						tipo: "estado",
+						valor: valor["USUARIO_ACTIVADO"],
+						img: "",
+						subClase: valor["USUARIO_ACTIVADO"],
+						operacion: "",
+						parametro: ""
+					}, {
+						key: valor["CEDULA"],
+						id: valor["CEDULA"],
+						tipo: "boton",
+						valor: "Modificar estado",
+						img: "",
+						subClase: "modificar",
+						operacion: modificarEstadoEmpleado,
+						parametro: valor["USUARIO_ACTIVADO"] == 1 ? 0 : 1
+					}, {
+						key: valor["CEDULA"],
+						id: valor["CEDULA"],
+						tipo: "boton",
+						valor: "Nueva clave",
+						img: "",
+						subClase: "userUpdate",
+						operacion: modificarClaveEmpleado,
+						parametro: valor["USUARIO_ACTIVADO"] == 1 ? 0 : 1
+					}, {
+						key: valor["CEDULA"],
+						id: valor["CEDULA"],
+						tipo: "boton",
+						valor: "Eliminar empleado",
+						img: "",
+						subClase: "basurero",
+						operacion: eliminarEmpleadoSistema,
+						parametro: valor["USUARIO_ACTIVADO"] == 1 ? 0 : 1
+					}]
+				);
+				if (nuevosDatos.length > 0) setDatosMain(nuevosDatos);
+				if (nuevosDatos.length <= 0) setDatosMain([[]]);
+			}).catch(function (error) {
+				setDatosMain([[]]);
+			});
 	}
 
 	/*-- Modificar Empleado --*/
-	const modificarEstadoEmpleado = async (cedula, dato) =>{
+	const modificarEstadoEmpleado = async (cedula, dato) => {
 		const nuevoEstado = {
 			cedula: cedula,
 			estado: dato,
 		}
 
 		classAdmin.modificarEstadoUsuario(nuevoEstado)
-		.then(resultado =>{
-			if(resultado === false){
-				console.clear();
-				alertaToast("warning", "Ocurrio un error", 5000, "top-end");
-				throw new Error("No hay resultados almacenados");
-			}
-			alertaToast(
-				resultado[0]["RESULTADO"].toUpperCase().includes("ERROR") ? "error" : "success", 
-				resultado[0]["RESULTADO"], 
-				5000, 
-				"top-end");
-			generarLista();
-		}).catch(function (error) {
-			console.error(error);
-		});
+			.then(resultado => {
+				if (resultado === false) {
+					alertaToast("warning", "Ocurrió un error", 5000, "top-end");
+					throw new Error("No hay resultados almacenados");
+				}
+				alertaToast(
+					resultado[0]["RESULTADO"].toUpperCase().includes("ERROR") ? "error" : "success",
+					resultado[0]["RESULTADO"],
+					5000,
+					"top-end");
+				generarLista();
+			}).catch(function (error) {
+				return error;
+			});
 	}
 
 	/*-- Eliminar Empleado --*/
-	const eliminarEstadoEmpleado = async (cedula, dato) =>{
+	const eliminarEmpleadoSistema = async (cedula, dato) => {
 		alertaConfirmar(
-			"Segur@ que desea eliminar el empleado", 
-			`El empleado con el numero de cedula "${cedula}" sera eliminado si no cuenta con ventas registradas`, 
+			"Segur@ que desea eliminar el empleado",
+			`El empleado con el número de cédula "${cedula}" será eliminado si no cuenta con ventas registradas`,
 			"warning")
-		.then(confirm =>{
+			.then(confirm => {
 
-			if(confirm){
-				classAdmin.eliminarEmpleado(cedula)
-				.then(() =>{
-			
-					classAdmin.usuarios(cedula)
-					.then(resultado =>{
-						console.log(resultado);
-						if(resultado.length == 0){
-							alertaToast(
-								"success", 
-								"EL USUARIO HA SIDO ELIMINADO", 
-								5000, 
-								"top-end");
-								generarLista();
-						}else{
-							alertaToast("warning", "EL USUARIO NO SE PUDO ELIMINAR", 5000, "top-end");
-						}
-					});
-		    });
-			}
-			console.log(confirm)
-		}).catch(function (error) {
-			console.error(error);
-		});
-		
+				if (confirm) {
+					classAdmin.eliminarEmpleado(cedula)
+						.then(() => {
+							classAdmin.usuarios(cedula)
+								.then(resultado => {
+									if (resultado.length == 0) {
+										alertaToast(
+											"success",
+											"El usuario ha sido eliminado",
+											5000,
+											"top-end");
+										generarLista();
+									} else {
+										alertaToast("warning", "El usuario no se pudo eliminado", 5000, "top-end");
+									}
+								});
+						});
+				}
+			}).catch(function (error) {
+				return error;
+			});
+
 	}
 
-  return(
+	/*-- modificar clave Empleado --*/
+	const modificarClaveEmpleado = async (cedula, dato) => {
+		alertaConfirmar(
+			"Segur@ que desea modificar la clave del empleado",
+			`CC: "${cedula}"`,
+			"warning")
+			.then(confirm => {
+				if (confirm) {
+					alertaInput()
+						.then(resultado => {
+							if (resultado == undefined || resultado == "") throw new Error("No se ingreso un valor valido")
+							let nuevosDatos = {
+								cedula: cedula,
+								clave: resultado
+							}
+							classAdmin.cambiarClave(nuevosDatos)
+								.then((resultado) => {
+									alertaToast(
+										"success",
+										`${resultado[0]["RESULTADO"]}`,
+										5000,
+										"top-end");
+								})
+						}).catch(function (error) {
+							return error;
+						});
+				}
+			}).catch(function (error) {
+				return error;
+			});
+
+	}
+
+	return (
 		<>
-			<TablaMain 
-				buscadorNombre="Cedula o nombre" 
-				buscadorTitulo="Empleados"  
+			<TablaMain
+				buscadorNombre="Cedula o nombre"
+				buscadorTitulo="Empleados"
 				actualizar={generarLista}
 				tablaTitulos={titulosTabla}
-				tablaContenido={datosMain} 
+				tablaContenido={datosMain}
 				oculto={true}>
 			</TablaMain>
 		</>
