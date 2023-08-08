e3dcd5fe4d0dded10424a59bd4c8ef021b70fe0f
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-08-08 12:52:11 -0500

    Sub opcion.
    Retorna un elemento con la sub opción.
    
    props:
    - nombre: el nombre de la sub Opcion.
    
    - ruta: la ruta de la opción.

--- src/components/view/menus/components/subOpcion.jsx
+++ src/components/view/menus/components/subOpcion.jsx
@@ -1,13 +1,18 @@
-import React from "react";
-import { Link } from "react-router-dom";
+/*-- Estilos --*/
 import "../../../style/menus/components/opcion.css";
 
-export function SubOpcionMenu({nombre, ruta}) {
-  return(
+/*-- Librerias --*/
+import React from "react";
+
+/*-- Componentes --*/
+import { Link } from "react-router-dom";
+
+export function SubOpcionMenu({ nombre, ruta }) {
+	return (
 		<>
 			<li className="lista__subOpciones">
 				<Link to={ruta} className="opcion__link opcion__link--inside">{nombre}</Link>
-      		</li>
+			</li>
 		</>
 	);
 }
