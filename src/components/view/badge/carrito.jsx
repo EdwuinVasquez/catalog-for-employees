1126bcdeaaa568eff1d99dc2b70da06a12a4e751
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-08-08 10:15:05 -0500

    badge de carrito en catálogo.
    Contiene un badge con un icono de carrito y el número de ítems
    vinculados, funciona como un botón que despliega el carrito.
    
    Funciones:
    - calcularItems: calcula el número de ítems en el carrito y los retorna.

--- src/components/view/badge/carrito.jsx
+++ src/components/view/badge/carrito.jsx
@@ -6,9 +6,9 @@ import { BsCart3 } from "react-icons/bs";
 /*-- clases y controladores --*/
 import { useDataContex } from "../contex";
 
-export function CarritoBoton({manejarClick}) {
+export function CarritoBoton({ manejarClick }) {
 	/*-- Estado global --*/
-	const { carrito, setContexMenu } =  useDataContex();
+	const { carrito, setContexMenu } = useDataContex();
 
 	/*-- Activar visor de carrito --*/
 	const procesarClick = (() => {
@@ -17,18 +17,26 @@ export function CarritoBoton({manejarClick}) {
 	})
 
 	/*-- Calcular numero de items --*/
-	const calcularItems = (() =>{
-		const total = carrito.reduce((valorAnterior, valorActual) => {
-			return valorAnterior + valorActual[0]["cantidad"];
-		}, 0);
-		return total;
+	const calcularItems = (() => {
+		try {
+			if (carrito[0] != undefined) {
+				const total = carrito.reduce((valorAnterior, valorActual) => {
+					return valorAnterior + parseInt(valorActual[0]["cantidad"]);
+				}, 0);
+				return total;
+			} else {
+				return 0;
+			}
+		} catch (error) {
+			return 0;
+		}
 	});
 
-  return(
-	<div onClick={procesarClick}>
-		<Badge color="primary" badgeContent={calcularItems()} showZero>
-			<BsCart3></BsCart3>
-		</Badge>
-	</div>
+	return (
+		<div onClick={procesarClick}>
+			<Badge color="primary" badgeContent={calcularItems()} showZero>
+				<BsCart3></BsCart3>
+			</Badge>
+		</div>
 	);
 };
