46476dc6da3385eb60a73fbd8b897893ce5ec47f
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-08-08 13:38:17 -0500

    Lista de datos de la tabla.
    Muestra todos los datos de la tabla. Genera las filas que lleva la tabla
    a partir de la información suministrada en los props y con ayuda del
    componente "TablaBody".
    
    props:
    - titulos: los titulos o cabeceras de la tabla.
    
    - contenido: información o datos de cada fila de la tabla.
    
    - actualizar: proceso para actualizar los datos de la tabla.
    
    Funciones:
    - datos: genera las filas a partir del prop contenido

--- src/components/view/tablas/tablaMain/componTabla/contenido.jsx
+++ src/components/view/tablas/tablaMain/componTabla/contenido.jsx
@@ -3,39 +3,40 @@ import "../../../../style/tabla/tablaMain/contenido.css"
 
 /*-- Importaciones --*/
 import { React } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 /*-- Componentes --*/
 import { TablaTitulos } from "./titulos";
 import { TablaBody } from "./datos";
 
-export function TablaCotenido({titulos, contenido, actualizar}) {
+export function TablaCotenido({ titulos, contenido, actualizar }) {
   /*-- Generar tuplas --*/
-  const datos = (() =>{
+  const datos = (() => {
     try {
-      if(contenido[0].length > 2){
+      if (contenido[0].length > 2) {
         return contenido.map((valor) =>
-          <TablaBody datos={valor}></TablaBody>
+          <TablaBody key={uuidv4()} datos={valor}></TablaBody>
         )
-      }else{
-        return <h1>No hay datos almacenados</h1>
+      } else {
+        return <h1 key={uuidv4()}>No hay datos almacenados</h1>
       }
     } catch (error) {
       actualizar();
     }
   });
 
-  return(
+  return (
     <>
-    <section className="tabla__contenido" id="tabla__contenido--export">
-      <table>
-        <TablaTitulos titulos={titulos}></TablaTitulos>
-        <tbody>
-          {
-            datos()
-          }
-        </tbody>
-      </table>
-		</section>
-		</>
-	);
+      <section className="tabla__contenido" id="tabla__contenido--export">
+        <table>
+          <TablaTitulos titulos={titulos}></TablaTitulos>
+          <tbody>
+            {
+              datos()
+            }
+          </tbody>
+        </table>
+      </section>
+    </>
+  );
 };
