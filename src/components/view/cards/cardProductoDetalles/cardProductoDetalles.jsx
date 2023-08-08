2eb17f44a1058b1d29b5e6810756e7b9942fb59c
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-08-08 15:44:28 -0500

    Tarjeta de detalles.
    Esta tarjeta muestra todos los detalles de un producto específico.
    Se usa cuando se da clic sobre la tarjeta de básica del producto.
    
    props:
    - nombre: nombre del producto.
    
    - detalles: detalles del producto.
    
    - lista: lista de los estilos del producto.
    
    - valor: valor de venta.
    
    - valorBase: valor sin aplicar los descuentos.
    
    - setEstadoDetalle: modificador de la visión de los detalles.
    
    Funciones:
    - manejarCambioEstilo: Obtiene el estilo seleccionado por el usuario
    en esta tarjeta.

--- src/components/view/cards/cardProductoDetalles/cardProductoDetalles.jsx
+++ src/components/view/cards/cardProductoDetalles/cardProductoDetalles.jsx
@@ -12,28 +12,27 @@ import { FinalCardProductoDetalles } from './components/finalCardDetalles';
 import { EstilosCardProductoDetalles } from './components/estilosCardDetalles';
 import { CerrarCardProductoDetalles } from './components/cerrarCardDetalles';
 
-export function CardProductoDetalles({nombre, detalles, lista, imagen, valor, valorBase, setEstadoDetalle}) {
+export function CardProductoDetalles({ nombre, detalles, lista, imagen, valor, valorBase, setEstadoDetalle }) {
 	/*-- Estado producto con estilo --*/
 	const [productoEstilo, setProductoEstilo] = useState("0");
-	
+
 	/*-- Estado imagen del estilo --*/
 	const [imagenItem, setImagenItem] = useState(imagen);
 
-	const manejarCambioEstilo = ((datos) =>{
-		if(datos[0]["disponible"] == 1){
+	const manejarCambioEstilo = ((datos) => {
+		if (datos[0]["disponible"] == 1) {
 			setProductoEstilo(datos);
 			setImagenItem(datos[0]["imgItem"]);
 		}
 	});
 
-	return(
+	return (
 		<div className="cardProductoDetalles">
 			<CerrarCardProductoDetalles cerrarDetalles={setEstadoDetalle}></CerrarCardProductoDetalles>
-      <ImagenCardProductoDetalles url={imagenItem}></ImagenCardProductoDetalles>
-      <InformacionCardProductoDetalles titulo={nombre} detalles={detalles}></InformacionCardProductoDetalles>
+			<ImagenCardProductoDetalles url={imagenItem}></ImagenCardProductoDetalles>
+			<InformacionCardProductoDetalles titulo={nombre} detalles={detalles}></InformacionCardProductoDetalles>
 			<EstilosCardProductoDetalles lista={lista} manejarCambioEstilo={manejarCambioEstilo}></EstilosCardProductoDetalles>
-      <FinalCardProductoDetalles precio={valor} precioBase={valorBase} productoSelect={productoEstilo}></FinalCardProductoDetalles>
+			<FinalCardProductoDetalles precio={valor} precioBase={valorBase} productoSelect={productoEstilo}></FinalCardProductoDetalles>
 		</div>
 	);
 };
-
