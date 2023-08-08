39b0147f31a98c51321936427ef0a9c82efa7c0f
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-08-08 13:02:03 -0500

    Botones tipo icono en la tabla.
    Retorna un icono con una función integrada.
    
    props:
    - operacion: recibe una función a ejecutar al presionar clic sobre el
    icono.
    
    - id: recibe el identificador de lo que debe ser modificado.
    
    - parametro: recibe los valores de cambio.
    
    - icono: recibe el nombre del icono que debe llevar el botón.
    
    - nombre: recibe el nombre del botón.
    
    Funciones:
    - tipoBoton: asigna el icono seleccionado al botón al igual que vincula
    la función con sus respectivos parámetros.

--- src/components/view/tablas/tablaMain/componTabla/boton.jsx
+++ src/components/view/tablas/tablaMain/componTabla/boton.jsx
@@ -1,56 +1,88 @@
 /*-- Estilos --*/
-import "../../../../style/tabla/tablaMain/boton.css"
+import "../../../../style/tabla/tablaMain/boton.css";
 
 /*-- Importacion --*/
 import { React } from "react";
 import Tooltip from '@mui/material/Tooltip';
 import { RxUpdate } from "react-icons/rx";
 import { RiLoginCircleFill } from "react-icons/ri";
-import { MdDelete } from "react-icons/md";
-import { IoIosEye } from "react-icons/io";
-import { BsFillPersonCheckFill } from "react-icons/bs";
+import { MdDelete, MdOutlinePriceCheck, MdPriceChange } from "react-icons/md";
+import { IoIosEye, IoIosPricetags } from "react-icons/io";
+import { BsFillPersonCheckFill, BsPercent } from "react-icons/bs";
+import { BiRename } from "react-icons/bi";
+import { AiOutlineUserSwitch } from "react-icons/ai";
 
-export function BotonTabla({operacion, id, parametro, icono, nombre}) {
+export function BotonTabla({ operacion, id, parametro, icono, nombre }) {
 	/*-- Asignar retorno --*/
 	const tipoBoton = (key) => {
 		switch (key) {
-			case "RxUpdate":
-				return <RxUpdate  
+			case "modificar":
+				return <RxUpdate
 					className="botonTabla__icono  botonTabla__icono--negro"
-					onClick={() => operacion(id, parametro)} 
-					> </RxUpdate>
-				case "MdDelete": 
-				  return <MdDelete 
+					onClick={() => operacion(id, parametro)}
+				> </RxUpdate>
+			case "basurero":
+				return <MdDelete
 					className="botonTabla__icono  botonTabla__icono--rojo"
-					onClick={() => operacion(id, parametro)} 
-					> </MdDelete>
-				case "RiLoginCircleFill": 
-				  return <RiLoginCircleFill 
+					onClick={() => operacion(id, parametro)}
+				> </MdDelete>
+			case "login":
+				return <RiLoginCircleFill
 					className="botonTabla__icono  botonTabla__icono--verde"
-					onClick={() => operacion(id, parametro)} 
-					> </RiLoginCircleFill>
-				case "BsFillPersonCheckFill": 
-				  return <BsFillPersonCheckFill 
+					onClick={() => operacion(id, parametro)}
+				> </RiLoginCircleFill>
+			case "usuarioCheck":
+				return <BsFillPersonCheckFill
 					className="botonTabla__icono  botonTabla__icono--negro"
-					onClick={() => operacion(id, parametro)} 
-					> </BsFillPersonCheckFill>
-				case "IoIosEye": 
-					return <IoIosEye 
-					  className="botonTabla__icono  botonTabla__icono--negro"
-					  onClick={() => operacion(id, parametro)} 
-					  > </IoIosEye>
+					onClick={() => operacion(id, parametro)}
+				> </BsFillPersonCheckFill>
+			case "ojo":
+				return <IoIosEye
+					className="botonTabla__icono  botonTabla__icono--negro"
+					onClick={() => operacion(id, parametro)}
+				> </IoIosEye>
+			case "porcentaje":
+				return <BsPercent
+					className="botonTabla__icono  botonTabla__icono--negro"
+					onClick={() => operacion(id, parametro)}
+				> </BsPercent>
+			case "valor":
+				return <MdOutlinePriceCheck
+					className="botonTabla__icono  botonTabla__icono--negro"
+					onClick={() => operacion(id, parametro)}
+				> </MdOutlinePriceCheck>
+			case "iva":
+				return <IoIosPricetags
+					className="botonTabla__icono  botonTabla__icono--negro"
+					onClick={() => operacion(id, parametro)}
+				> </IoIosPricetags>
+			case "descuento":
+				return <MdPriceChange
+					className="botonTabla__icono  botonTabla__icono--negro"
+					onClick={() => operacion(id, parametro)}
+				> </MdPriceChange>
+			case "renombrar":
+				return <BiRename
+					className="botonTabla__icono  botonTabla__icono--negro"
+					onClick={() => operacion(id, parametro)}
+				> </BiRename>
+			case "userUpdate":
+				return <AiOutlineUserSwitch
+					className="botonTabla__icono  botonTabla__icono--negro"
+					onClick={() => operacion(id, parametro)}
+				> </AiOutlineUserSwitch>
 			default:
 				return <></>
 		}
 	}
 
-  return(
-		<>		
-		<Tooltip title={nombre} placement="top">
-			<div className="botonTabla">
-				{tipoBoton(icono)}
-			</div>
-		</Tooltip>
+	return (
+		<>
+			<Tooltip title={nombre} placement="top">
+				<div className="botonTabla">
+					{tipoBoton(icono)}
+				</div>
+			</Tooltip>
 		</>
 	);
 };
