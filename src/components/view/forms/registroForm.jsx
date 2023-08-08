960e5f5a23211d3487ecb1f5caf6eb92918a548d
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-08-08 12:35:11 -0500

    Formulario de registro.
    Despliega un formulario de registro. Este formulario sirve para
    registrar nuevas solicitudes de usuario.
    
    Funciones:
    - actualizarDatos: actualiza los datos de un input en el estado,
    recibe el nuevo dado, si está en el formato correcto y el key o id
    del input desde el cual recibe la información.
    
    - todosLosDatosSonCorrectos: evalúa que todos los campos estén
    correctos y llenados; retorna el resultado de la operación.
    
    - limpiarFormulario: limpia todos los campos del formulario.
    
    - registarse: envía la petición del nuevo usuario a la API.

--- src/components/view/forms/registroForm.jsx
+++ src/components/view/forms/registroForm.jsx
@@ -2,25 +2,24 @@
 import '../../style/forms/registroForm.css';
 
 /*-- Librerias --*/
-import { React, useState } from "react";
-import { Route, Routes, Link } from "react-router-dom";
+import { React, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 /*-- Componentes --*/
 import { BotonSencillo } from "../botones/botonSencillo.jsx";
 import { TituloInicio } from "../titulos/tituloInicio.jsx";
 import { CampoVerificado } from "../inputs/inputBase.jsx";
-import { LoginFomulario } from "./inicioSesionForm.jsx";
 import { SelectBase } from "../inputs/selectBase.jsx";
 
 /*-- Clases y controladores --*/
 import { regex } from "../../../backend/regex.js";
 import { usuario } from "../../../backend/class/usuario.js";
 import { alertaToast } from '../../../backend/swetAlert2';
+import { listaOpciones } from '../../../backend/funcioneGenerales';
 const classUsuario = new usuario();
 
 export function RegistroFomulario() {
-
-	/*-- Value de los campos --*/
+	/*-- Aqui se almacenan los valores de los campos del formularios --*/
 	const [datos, setDatos] = useState({
 		cedula: "",
 		nombre: "",
@@ -35,8 +34,8 @@ export function RegistroFomulario() {
 		contacto: ""
 	});
 
-	/*-- Validez del dato --*/
-	const [formCorrecto, setFormCorrecto] = useState({
+	/*-- Aqui se almacena un boleano de cada dato que permite conocer si estan en el formato indicado  --*/
+	const [datosCorrectos, setDatosCorrectos] = useState({
 		nombres: false,
 		apellidos: false,
 		cedula: false,
@@ -46,53 +45,38 @@ export function RegistroFomulario() {
 		clave: false
 	});
 
-	/*-- Lista de opciones para pregunta de seguridad */
-	const listaOpciones = [
-		[{key: 1, valor: "En que ciudad naciste"}],
-		[{key: 2, valor: "Cual es tu equipo deportivo favorito"}],
-		[{key: 3, valor: "Cual es el nombre de tu ciudad natal"}],
-		[{key: 4, valor: "Cual es tu deporte favorito"}],
-		[{key: 5, valor: "Nombre de tu ultima mascota"}],
-		[{key: 6, valor: "Cual es tu numero de identificación personal"}],
-		[{key: 7, valor: "En que mes naciste"}],
-		[{key: 8, valor: "En que año empezaste a trabajar en esta empresa"}],
-		[{key: 9, valor: "Cual es tu numero de telefono"}],
-		[{key: 10, valor: "En que dia naciste"}],
-		[{key: 11, valor: "Cual es tu posicion en la linea de produccion"}],
-		[{key: 12, valor: "Cual es el nombre de tu supervisor directo"}]
-	];
-
-	/*-- Verifica todos los campos y retorna Booleano */
-	const verificarEnvio = () => {
-		for (let key in formCorrecto) {
-			if (formCorrecto[key] === false){
+	/*-- Aqui validamos que todo este llenado correctamente */
+	const todosLosDatosSonCorrectos = () => {
+		for (let key in datosCorrectos) {
+			if (datosCorrectos[key] === false) {
 				return false;
-			}};
-			return true;
+			}
+		};
+		return true;
 	};
 
-	/*-- Actualiza el estado de los datos con el nuevo valor*/
-	const actualizarDatos = (dato, correcto, key) =>{
+	/*-- Actualiza el valor del dato del formulario en el estado --*/
+	const actualizarDatos = (dato, correcto, key) => {
 		setDatos(() => {
 			let nuevosDatos = datos;
 			nuevosDatos[key] = dato;
-			if(key == "nombres" || key == "apellidos") nuevosDatos["nombre"] = `${nuevosDatos["nombres"]} ${nuevosDatos["apellidos"]}`;
+			if (key == "nombres" || key == "apellidos") nuevosDatos["nombre"] = `${nuevosDatos["nombres"]} ${nuevosDatos["apellidos"]}`;
 			return nuevosDatos;
 		});
 
-		setFormCorrecto(() => {
-			let nuevosDatos = formCorrecto;
+		setDatosCorrectos(() => {
+			let nuevosDatos = datosCorrectos;
 			nuevosDatos[key] = correcto;
 			return nuevosDatos;
 		});
-  	}
+	}
 
-	/*-- limpiar formulario --*/
-	const limpiarFormulario = () =>{	
+	/*-- limpia los datos del formulario --*/
+	const limpiarFormulario = () => {
 		let formulario = document.querySelectorAll(".registroFomulario div input");
-		formulario.forEach(input => input.value="")
+		formulario.forEach(input => input.value = "")
 
-		setFormCorrecto({
+		setDatosCorrectos({
 			nombres: false,
 			apellidos: false,
 			cedula: false,
@@ -103,100 +87,99 @@ export function RegistroFomulario() {
 		})
 	}
 
-	/*-- Eniva el formulario --*/
-	const registarse = async (e) =>{
+	/*-- Eniva el formulario a la api para su procesamiento --*/
+	const registarse = async (e) => {
 		e.preventDefault();
-		if(verificarEnvio()) {
+		if (todosLosDatosSonCorrectos()) {
 			classUsuario.registarse(datos)
-			.then(resultado => {
-				if(resultado == false){
-					alertaToast("warning", "Ocurrio un error", 5000, "top-end");
-					throw new Error("Ocurrio un error");
-				}
-				alertaToast(
-					resultado[0]["RESULTADO"].toUpperCase().includes("ERROR") ? "error" : "success", 
-					resultado[0]["RESULTADO"].toUpperCase().includes("ERROR") ? resultado[0]["RESULTADO"] : `${ resultado[0]["RESULTADO"]}, COMUNIQUESE CON UN ADMINISTRADOR PARA SU VERIFICACION`, 
-					5000, 
-					"top-end");
-				if(resultado[0]["RESULTADO"].toUpperCase().includes("ERROR") ? false : true){
-					limpiarFormulario();
-				}
-			}).catch(function (error) {
-				console.error(error);
-			});
-		}else{
-			alertaToast("warning", "Aun hay datos sin llenar o incorrectos", 5000, "top-end");
+				.then(resultado => {
+					if (resultado == false) {
+						alertaToast("warning", "Ocurrio un error", 5000, "top-end");
+						throw new Error("Ocurrio un error");
+					}
+					alertaToast(
+						resultado[0]["RESULTADO"].toUpperCase().includes("ERROR") ? "error" : "success",
+						resultado[0]["RESULTADO"].toUpperCase().includes("ERROR") ? resultado[0]["RESULTADO"] : `${resultado[0]["RESULTADO"]}, COMUNIQUESE CON UN ADMINISTRADOR PARA SU VERIFICACION`,
+						5000,
+						"top-end");
+					if (resultado[0]["RESULTADO"].toUpperCase().includes("ERROR") ? false : true) {
+						limpiarFormulario();
+					}
+				}).catch(function (error) {
+					console.error(error);
+				});
+		} else {
+			alertaToast("warning", "Aún hay datos sin llenar o incorrectos", 5000, "top-end");
 		}
-  	}
+	}
 
+	useEffect(() => {
+		localStorage.removeItem("usuario");
+		localStorage.removeItem("carritoCopia");
+	});
 
-  return(
+	return (
 		<>
-		<div className="contenedor--centrado">
-			<form className="registroFomulario">
-				<TituloInicio texto="Registarse"></TituloInicio>
-				<div className="registroFomulario__flex">
-					<CampoVerificado 
-						id="nombres" 
-						expresion={regex.nombre} 
-						texto="Nombre" 
-						tipo="text" 
-						manejarCambio={actualizarDatos}
+			<div className="contenedor--centrado" style={{ position: "absolute", top: "0" }}>
+				<form className="registroFomulario">
+					<TituloInicio texto="Registarse"></TituloInicio>
+					<div className="registroFomulario__flex">
+						<CampoVerificado
+							id="nombres"
+							expresion={regex.nombre}
+							texto="Nombre"
+							tipo="text"
+							manejarCambio={actualizarDatos}
 						></CampoVerificado>
-					<CampoVerificado 
-						id="apellidos" 
-						expresion={regex.nombre} 
-						texto="Apellido" 
+						<CampoVerificado
+							id="apellidos"
+							expresion={regex.nombre}
+							texto="Apellido"
+							tipo="text"
+							manejarCambio={actualizarDatos}
+						></CampoVerificado>
+					</div>
+					<CampoVerificado
+						id="cedula"
+						expresion={regex.cedula}
+						texto="Cedula"
+						tipo="number"
+						manejarCambio={actualizarDatos}
+					></CampoVerificado>
+					<CampoVerificado
+						id="contacto"
+						expresion={regex.contacto}
+						texto="Numero de Celular"
 						tipo="text"
 						manejarCambio={actualizarDatos}
-						></CampoVerificado>
-				</div>
-				<CampoVerificado 
-					id="cedula" 
-					expresion={regex.cedula} 
-					texto="Cedula" 
-					tipo="number" 
-					manejarCambio={actualizarDatos}
 					></CampoVerificado>
-				<CampoVerificado 
-					id="contacto" 
-					expresion={regex.contacto} 
-					texto="Numero de Celular" 
-					tipo="text"
-					manejarCambio={actualizarDatos}
-					></CampoVerificado>
-				<SelectBase 
-						id="pregunta" 
-						expresion={regex.pregunta} 
-						texto="Pregunta de seguridad" 
+					<SelectBase
+						id="pregunta"
+						expresion={regex.pregunta}
+						texto="Pregunta de seguridad"
 						listaOpciones={listaOpciones}
 						manejarCambio={actualizarDatos}
-						></SelectBase>
-					<CampoVerificado 
-						id="respuesta" 
-						expresion={regex.respuesta} 
-						texto="Respuesta de seguridad" 
-						tipo="text"
+					></SelectBase>
+					<CampoVerificado
+						id="respuesta"
+						expresion={regex.respuesta}
+						texto="Respuesta de seguridad"
+						tipo="password"
 						manejarCambio={actualizarDatos}
-						></CampoVerificado>
-				<CampoVerificado 
-					id="clave" 
-					expresion={regex.clave} 
-					texto="Clave" 
-					tipo="password"
-					manejarCambio={actualizarDatos}
 					></CampoVerificado>
-				<BotonSencillo texto="Registrarse" manejarClik={registarse}></BotonSencillo>
-				<Link to="/login">
-					<BotonSencillo texto="Ingresar" manejarClik={() => {console.log("a")}}></BotonSencillo>
-				</Link>
-			</form>
-
-			<Routes>
-				<Route exact path='/login/*' element={<LoginFomulario />}/>
-				<Route path="*" element={null}/>
-    	</Routes>
-		</div>
+					<CampoVerificado
+						id="clave"
+						expresion={regex.clave}
+						texto="Clave"
+						tipo="password"
+						manejarCambio={actualizarDatos}
+					></CampoVerificado>
+					<BotonSencillo texto="Registrarse" manejarClik={registarse}></BotonSencillo>
+					<Link to="/login">
+						<BotonSencillo texto="Ingresar" manejarClik={() => console.log()}></BotonSencillo>
+					</Link>
+				</form>
+			</div>
 		</>
 	);
 };
