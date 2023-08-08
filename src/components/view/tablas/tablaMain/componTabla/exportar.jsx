36583a640dc98d54fed8da8695699b4933f1a67a
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-08-08 13:24:35 -0500

    Exportación de datos de la tabla.
    Muestra los botones para la importación de los datos.
    
    props:
    - oculto: Booleano de sí la exportación está activa o no.
    
    - nombre: nombre por defecto del archivo a exportar.
    
    - header: títulos de la tabla en Excel.
    
    - body: contenido de la tabla en Excel.
    
    - bodyCsv: contenido de exportación en formato CSV.
    
    -bodyJson: contenido de exportación en formato JSON.
    
    Funciones:
    - tipoBloque: retorna el bloque de exportación o un div vacío.
    
    - descargarEcxel: realiza la exportación en Excel.

--- src/components/view/tablas/tablaMain/componTabla/exportar.jsx
+++ src/components/view/tablas/tablaMain/componTabla/exportar.jsx
@@ -1,30 +1,32 @@
-//importacion de librerias
+/*-- Estilo --*/
+import "../../../../style/tabla/tablaMain/exportar.css";
+
+/*-- Librerias --*/
 import { React } from "react";
 import { downloadExcel } from "react-export-table-to-excel";
 import { Pdf, json, csv, excel } from "../../../logos/img.jsx";
-import "../../../../style/tabla/tablaMain/exportar.css"
 import { CSVLink } from "react-csv";
 
-export function TablaExportar({oculto, nombre, header, body, bodyCsv, bodyJson}) {
+export function TablaExportar({ oculto, nombre, header, body, bodyCsv, bodyJson }) {
   /*-- Bloque oculto --*/
-  const tipoBloque = (bloque) =>{
-    if(bloque){
+  const tipoBloque = (bloque) => {
+    if (bloque) {
       return <>
-      <div></div>
+        <div></div>
       </>
-    }else{
+    } else {
       return <>
-      <div htmlFor="export-file" className="exportar">
-        <label htmlFor="export-file" className="exportar__boton" title="Export File"></label>
-        <input className="exportar__input" type="checkbox" id="export-file" />
-        <div htmlFor="export-file" className="exportar__opciones">
-          <label className="exportar__label  exportar__label--fir">Export As &nbsp; &#10140;</label>
-          <label className="exportar__label" id="toPDF">PDF <Pdf></Pdf> </label>
-          <CSVLink data={bodyJson} filename={`${nombre}.json`} style={{color: "#000"}}>  <label className="exportar__label" id="toJSON">JSON <img src={json} alt="" /></label> </CSVLink>
-          <CSVLink data={bodyCsv} filename={`${nombre}.csv`} style={{color: "#000"}}>  <label className="exportar__label" id="toCSV"> CSV <img src={csv} alt="" /></label> </CSVLink>
-          <label className="exportar__label" id="toEXCEL" onClick={descargarEcxel} >EXCEL <img src={excel} alt="" /></label>
+        <div htmlFor="export-file" className="exportar">
+          <label htmlFor="export-file" className="exportar__boton" title="Export File"></label>
+          <input className="exportar__input" type="checkbox" id="export-file" />
+          <div htmlFor="export-file" className="exportar__opciones">
+            <label className="exportar__label  exportar__label--fir">Export As &nbsp; &#10140;</label>
+            {/* <label className="exportar__label" id="toPDF">PDF <Pdf></Pdf> </label> */}
+            {/* <CSVLink data={bodyJson} filename={`${nombre}.json`} style={{ color: "#000" }}>  <label className="exportar__label" id="toJSON">JSON <img src={json} alt="" /></label> </CSVLink> */}
+            <CSVLink data={bodyCsv} filename={`${nombre}.csv`} style={{ color: "#000" }}>  <label className="exportar__label" id="toCSV"> CSV <img src={csv} alt="" /></label> </CSVLink>
+            <label className="exportar__label" id="toEXCEL" onClick={descargarEcxel} >EXCEL <img src={excel} alt="" /></label>
+          </div>
         </div>
-      </div>
       </>
     }
   }
@@ -40,11 +42,11 @@ export function TablaExportar({oculto, nombre, header, body, bodyCsv, bodyJson})
     });
   }
 
-  return(
+  return (
     <>
-    {
-      tipoBloque(oculto)
-    }
+      {
+        tipoBloque(oculto)
+      }
     </>
-	);
+  );
 };
