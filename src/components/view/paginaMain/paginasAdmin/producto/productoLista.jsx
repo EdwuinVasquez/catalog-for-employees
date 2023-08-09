a32bc651b9cb58c5a77c04aeb7b3578208113597
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-08-09 10:17:58 -0500

    Lista de productos.
    La lista de productos en una tabla.
    
    Funciones:
    - eliminarProductoSistema: elimina los productos si no están conectados
    a un producto.
    
    - generarLista: genera la lista de producto de los datos recibidos de la
    API.

--- src/components/view/paginaMain/paginasAdmin/producto/productoLista.jsx
+++ src/components/view/paginaMain/paginasAdmin/producto/productoLista.jsx
@@ -4,149 +4,164 @@ import { evaluate } from 'mathjs'
 import { TablaMain } from "../../../tablas/tablaMain/tablaMain.jsx";
 import { admin } from "../../../../../backend/class/admin.js"
 import { useDataContex } from "../../../contex.jsx"
+import { alertaConfirmar, alertaToast } from "../../../../../backend/swetAlert2.js";
+import { precioVenta } from "../../../../../backend/funcioneGenerales.js";
 const classAdmin = new admin();
 
 export function ProductoLista() {
 	const [datosMain, setDatosMain] = useState();
-	
-	const titulosTabla = ["Codigo", "Imagen", "Nombre", "Iva", "Descuento", "Precio producto", "Precio de venta", "Disponible", "Estado"]
 
-	const precioVenta = (precio, iva, descuento) =>{
-		return Math.ceil(evaluate(`(${precio}*1.${iva})-((${precio}*1.${iva})*${descuento}%)`));
+	const titulosTabla = ["Codigo", "Imagen", "Nombre", "Numero ventas", "Iva", "Descuento", "Precio producto", "Precio de venta", "Disponible", "Eliminar producto"]
+
+	/*-- Eliminar Empleado --*/
+	const eliminarProductoSistema = async (codigoProducto, dato) => {
+		alertaConfirmar(
+			"Segur@ que desea eliminar el producto",
+			`El producto con el número "${codigoProducto}" será eliminado si no cuenta con ventas registradas`,
+			"warning")
+			.then(confirm => {
+				if (confirm) {
+					classAdmin.eliminarProducto(codigoProducto)
+						.then(() => {
+							classAdmin.productoBase()
+								.then(resultado => {
+									alertaToast(
+										"success",
+										"Proceso finalizado",
+										5000,
+										"top-end");
+									generarLista();
+								});
+						});
+				}
+			}).catch(function (error) {
+				return error;
+			});
 	}
 
 	const generarLista = async () => {
 		classAdmin.productoBase()
-		.then(resultado => {
-			if(resultado === false){
-				console.clear();
-				throw new Error("No hay resultados almacenados");
-			}
-			let productosFiltrados = resultado.filter((tupla,index)=>{
-    			return resultado.indexOf(
+			.then(resultado => {
+				if (resultado === false) {
+					console.clear();
+					throw new Error("No hay resultados almacenados");
+				}
+				let productosFiltrados = resultado.filter((tupla, index) => {
+					return resultado.indexOf(
 						resultado.find(valor => valor["CODIGO_PRODUCTO"] == tupla["CODIGO_PRODUCTO"])
-						) === index;
-  			});
+					) === index;
+				});
 
-			const nuevosDatos = productosFiltrados.map((valor) =>
-				[{
-					key: valor["CODIGO_PRODUCTO"],
-					id: valor["CODIGO_PRODUCTO"],
-					tipo: "normal",
-					valor: valor["CODIGO_PRODUCTO"],
-					img: "",
-					subClase: "",
-					operacion: "",
-					parametro: ""
-				},{
-					key: valor["CODIGO_PRODUCTO"],
-					id: valor["CODIGO_PRODUCTO"],
-					tipo: "imag",
-					valor: valor["NOMBRE_PRODUCTO"],
-					img: valor["IMAGEN_PRINCIPAL"],
-					subClase: "",
-					operacion: "",
-					parametro: ""
-				},{
-					key: valor["CODIGO_PRODUCTO"],
-					id: valor["CODIGO_PRODUCTO"],
-					tipo: "normal",
-					valor: valor["NOMBRE_PRODUCTO"],
-					img: "",
-					subClase: "",
-					operacion: "",
-					parametro: ""
-				},{
-					key: valor["CODIGO_PRODUCTO"],
-					id: valor["CODIGO_PRODUCTO"],
-					tipo: "normal",
-					valor: valor["IVA"],
-					img: "",
-					subClase: "",
-					operacion: "",
-					parametro: ""
-				},{
-					key: valor["CODIGO_PRODUCTO"],
-					id: valor["CODIGO_PRODUCTO"],
-					tipo: "normal",
-					valor: valor["DESCUENTO"],
-					img: "",
-					subClase: "",
-					operacion: "",
-					parametro: ""
-				},{
-					key: valor["CODIGO_PRODUCTO"],
-					id: valor["CODIGO_PRODUCTO"],
-					tipo: "costo",
-					valor: valor["PRECIO_BASE"],
-					img: "",
-					subClase: "",
-					operacion: "",
-					parametro: ""
-				},{
-					key: valor["CODIGO_PRODUCTO"],
-					id: valor["CODIGO_PRODUCTO"],
-					tipo: "costo",
-					valor: precioVenta(valor["PRECIO_BASE"], valor["IVA"], valor["DESCUENTO"]),
-					img: "",
-					subClase: "",
-					operacion: "",
-					parametro: ""
-				},{
-					key: valor["CODIGO_PRODUCTO"],
-					id: valor["CODIGO_PRODUCTO"],
-					tipo: "estado",
-					valor: valor["PRODUCTO_DISPONIBLE"],
-					img: "",
-					subClase: valor["PRODUCTO_DISPONIBLE"],
-					operacion: "",
-					parametro: ""
-				},{
-					key: valor["CODIGO_PRODUCTO"],
-					id: valor["CODIGO_PRODUCTO"],
-					tipo: "estado",
-					valor: valor["ESTADO_PRODUCTO"],
-					img: "",
-					subClase: valor["ESTADO_PRODUCTO"],
-					operacion: "",
-					parametro: ""
-				}]
-			);
-			if(nuevosDatos.length > 0) setDatosMain(nuevosDatos);
-			if(nuevosDatos.length <= 0) setDatosMain([[]]);
-		}).catch(function (error) {
-			setDatosMain([[]]);
-			console.error(error);
-		});
+				const nuevosDatos = productosFiltrados.map((valor) =>
+					[{
+						key: valor["CODIGO_PRODUCTO"],
+						id: valor["CODIGO_PRODUCTO"],
+						tipo: "normal",
+						valor: valor["CODIGO_PRODUCTO"],
+						img: "",
+						subClase: "",
+						operacion: "",
+						parametro: ""
+					}, {
+						key: valor["CODIGO_PRODUCTO"],
+						id: valor["CODIGO_PRODUCTO"],
+						tipo: "imag",
+						valor: valor["NOMBRE_PRODUCTO"],
+						img: valor["IMAGEN_PRINCIPAL"],
+						subClase: "",
+						operacion: "",
+						parametro: ""
+					}, {
+						key: valor["CODIGO_PRODUCTO"],
+						id: valor["CODIGO_PRODUCTO"],
+						tipo: "normal",
+						valor: valor["NOMBRE_PRODUCTO"],
+						img: "",
+						subClase: "",
+						operacion: "",
+						parametro: ""
+					}, {
+						key: valor["CODIGO_PRODUCTO"],
+						id: valor["CODIGO_PRODUCTO"],
+						tipo: "normal",
+						valor: valor["NUMERO_VENTAS"],
+						img: "",
+						subClase: "",
+						operacion: "",
+						parametro: ""
+					}, {
+						key: valor["CODIGO_PRODUCTO"],
+						id: valor["CODIGO_PRODUCTO"],
+						tipo: "normal",
+						valor: valor["IVA"],
+						img: "",
+						subClase: "",
+						operacion: "",
+						parametro: ""
+					}, {
+						key: valor["CODIGO_PRODUCTO"],
+						id: valor["CODIGO_PRODUCTO"],
+						tipo: "normal",
+						valor: valor["DESCUENTO"],
+						img: "",
+						subClase: "",
+						operacion: "",
+						parametro: ""
+					}, {
+						key: valor["CODIGO_PRODUCTO"],
+						id: valor["CODIGO_PRODUCTO"],
+						tipo: "costo",
+						valor: valor["PRECIO_BASE"],
+						img: "",
+						subClase: "",
+						operacion: "",
+						parametro: ""
+					}, {
+						key: valor["CODIGO_PRODUCTO"],
+						id: valor["CODIGO_PRODUCTO"],
+						tipo: "costo",
+						valor: precioVenta(valor["PRECIO_BASE"], valor["IVA"], valor["DESCUENTO"]),
+						img: "",
+						subClase: "",
+						operacion: "",
+						parametro: ""
+					}, {
+						key: valor["CODIGO_PRODUCTO"],
+						id: valor["CODIGO_PRODUCTO"],
+						tipo: "estado",
+						valor: valor["PRODUCTO_DISPONIBLE"],
+						img: "",
+						subClase: valor["PRODUCTO_DISPONIBLE"],
+						operacion: "",
+						parametro: ""
+					}, {
+						key: valor["CODIGO_PRODUCTO"],
+						id: valor["CODIGO_PRODUCTO"],
+						tipo: "boton",
+						valor: "Eliminar producto",
+						img: "",
+						subClase: "basurero",
+						operacion: eliminarProductoSistema,
+						parametro: valor["CODIGO_PRODUCTO"]
+					}]
+				);
+				if (nuevosDatos.length > 0) setDatosMain(nuevosDatos);
+				if (nuevosDatos.length <= 0) setDatosMain([[]]);
+			}).catch(function (error) {
+				setDatosMain([[]]);
+				console.error(error);
+			});
 	}
 
-	const modificarEstadoEmpleado = async (cedula, dato) =>{
-		const nuevoEstado = {
-			cedula: cedula,
-			estado: dato,
-		}
-
-		classAdmin.modificarEstadoUsuario(nuevoEstado)
-		.then(resultado =>{
-			if(resultado === false){
-				console.clear();
-				throw new Error("No hay resultados almacenados");
-			}
-			
-			generarLista();
-		}).catch(function (error) {
-			console.error(error);
-		});
-	}
-
-  return(
+	return (
 		<>
-			<TablaMain 
-				buscadorNombre="Codigo o nombre" 
-				buscadorTitulo="Productos"  
+			<TablaMain
+				buscadorNombre="Codigo o nombre"
+				buscadorTitulo="Productos"
 				actualizar={generarLista}
 				tablaTitulos={titulosTabla}
-				tablaContenido={datosMain} >
+				tablaContenido={datosMain}
+				oculto={true}>
 			</TablaMain>
 		</>
 	);
